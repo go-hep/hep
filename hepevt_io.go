cb9b9457c2f1029d6eb25f38b74c534bd246b200
Sebastien Binet <binet@cern.ch>
2013-05-02 11:42:28 +0200

    hepevt: beautify doc

--- hepevt_io.go
+++ hepevt_io.go
@@ -1,57 +1,25 @@
 package hepevt
 
-/*
- #include <stdlib.h>
-*/
-import "C"
-
 import (
 	"fmt"
 )
 
-//const nmxhep = 4000
-
 // I/O ----------------------------------------------------------------------
 
-//////////////////////////////////////////////////////////////////////////
+// Event is the Go representation of the FORTRAN-77 HEPEVT common block:
 //
-//      PARAMETER (NMXHEP=2000) 
-//      COMMON/HEPEVT/NEVHEP,NHEP,ISTHEP(NMXHEP),IDHEP(NMXHEP), 
-//     &        JMOHEP(2,NMXHEP),JDAHEP(2,NMXHEP),PHEP(5,NMXHEP),VHEP(4,NMXHEP)
-/**********************************************************/
-/*           D E S C R I P T I O N :                      */
-/*--------------------------------------------------------*/
-/* NEVHEP          - event number (or some special meaning*/
-/*                    (see documentation for details)     */
-/* NHEP            - actual number of entries in current  */
-/*                    event.                              */
-/* ISTHEP[IHEP]    - status code for IHEP'th entry - see  */
-/*                    documentation for details           */
-/* IDHEP [IHEP]    - IHEP'th particle identifier according*/
-/*                    to PDG.                             */
-/* JMOHEP[IHEP][0] - pointer to position of 1st mother    */
-/* JMOHEP[IHEP][1] - pointer to position of 2nd mother    */
-/* JDAHEP[IHEP][0] - pointer to position of 1st daughter  */
-/* JDAHEP[IHEP][1] - pointer to position of 2nd daughter  */
-/* PHEP  [IHEP][0] - X momentum                           */
-/* PHEP  [IHEP][1] - Y momentum                           */
-/* PHEP  [IHEP][2] - Z momentum                           */
-/* PHEP  [IHEP][3] - Energy                               */
-/* PHEP  [IHEP][4] - Mass                                 */
-/* VHEP  [IHEP][0] - X vertex                             */
-/* VHEP  [IHEP][1] - Y vertex                             */
-/* VHEP  [IHEP][2] - Z vertex                             */
-/* VHEP  [IHEP][3] - production time                      */
-/*========================================================*/
+//  PARAMETER (NMXHEP=2000) 
+//  COMMON/HEPEVT/NEVHEP,NHEP,ISTHEP(NMXHEP),IDHEP(NMXHEP), 
+// &        JMOHEP(2,NMXHEP),JDAHEP(2,NMXHEP),PHEP(5,NMXHEP),VHEP(4,NMXHEP)
 type Event struct {
-	Nevhep int
-	Nhep   int
-	Isthep []int
-	Idhep  []int
-	Jmohep [][2]int
-	Jdahep [][2]int
-	Phep   [][5]float64
-	Vhep   [][4]float64
+	Nevhep int          // event number (or some special meaning, see doc for details)
+	Nhep   int          // actual number of entries in current event
+	Isthep []int        // status code for n'th entry
+	Idhep  []int        // particle identifier according to PDG
+	Jmohep [][2]int     // index of 1st and 2nd mother
+	Jdahep [][2]int     // index of 1st and 2nd daughter
+	Phep   [][5]float64 // particle 5-vector (px,py,pz,e,m)
+	Vhep   [][4]float64 // vertex 4-vector (x,y,z,t)
 }
 
 type Encoder interface {
