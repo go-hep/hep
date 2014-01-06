844db7b36cdc3893a50c532ab7074071ffcb4cb9
Sebastien Binet <binet@cern.ch>
2014-01-06 13:03:23 +0100

    buffer: make Key.DecodeVector also returns error

--- buffer.go
+++ buffer.go
@@ -15,7 +15,7 @@ type Basket struct {
 	Flag         byte
 }
 
-func (k *Key) DecodeVector(in *bytes.Buffer, dst interface{}) int {
+func (k *Key) DecodeVector(in *bytes.Buffer, dst interface{}) (int, error) {
 	// Discard three int16s (like 40 00 00 0e 00 09)
 	x := in.Next(6)
 	_ = x // sometimes we want to look at this.
@@ -23,12 +23,12 @@ func (k *Key) DecodeVector(in *bytes.Buffer, dst interface{}) int {
 	var n int32
 	err := B.Read(in, B.BigEndian, &n)
 	if err != nil {
-		panic(err)
+		return -1, err
 	}
 
 	err = B.Read(in, B.BigEndian, reflect.ValueOf(dst).Slice(0, int(n)).Interface())
 	if err != nil {
-		panic(err)
+		return -1, err
 	}
-	return int(n)
+	return int(n), nil
 }
