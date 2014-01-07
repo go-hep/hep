1e0ab6adc624b02349cdeed7818ec1e7e0b59492
Sebastien Binet <binet@cern.ch>
2014-01-07 17:56:21 +0100

    utils: datime2time

--- utils.go
+++ utils.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // decodeNameCycle decodes a namecycle "aap;2" into name "aap" and cycle "2"
@@ -30,6 +31,21 @@ func decodeNameCycle(namecycle string) (string, int16) {
 	}
 
 	return name, cycle
+}
+
+// datime2time converts a uint32 holding a ROOT's TDatime into a time.Time
+func datime2time(d uint32) time.Time {
+
+	// ROOT's TDatime begins in January 1995...
+	var year uint32 = (d >> 26) + 1995
+	var month uint32 = (d << 6) >> 28
+	var day uint32 = (d << 10) >> 27
+	var hour uint32 = (d << 15) >> 27
+	var min uint32 = (d << 20) >> 26
+	var sec uint32 = (d << 26) >> 26
+	nsec := 0
+	return time.Date(int(year), time.Month(month), int(day),
+		int(hour), int(min), int(sec), nsec, time.UTC)
 }
 
 // EOF
