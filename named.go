882c8eedd536207064d744290f7bb6b076d3bba9
Sebastien Binet <binet@cern.ch>
2014-01-10 18:42:57 +0100

    named: implement Object interface. make sure ROOTUnmarshaler is satisfied too

--- named.go
+++ named.go
@@ -12,6 +12,20 @@ type named struct {
 	title string
 }
 
+// Name returns the name of the instance
+func (n *named) Name() string {
+	return n.name
+}
+
+// Title returns the title of the instance
+func (n *named) Title() string {
+	return n.title
+}
+
+func (n *named) Class() string {
+	return "TNamed"
+}
+
 func (n *named) UnmarshalROOT(data []byte) error {
 	var err error
 	dec := rootDecoder{r: bytes.NewBuffer(data)}
@@ -28,5 +42,9 @@ func (n *named) UnmarshalROOT(data []byte) error {
 
 	return err
 }
+
+// testing interfaces
+var _ Object = (*named)(nil)
+var _ ROOTUnmarshaler = (*named)(nil)
 
 // EOF
