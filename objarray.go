096141a7f25b7c0529dae8c3d0b24fbec990c056
Sebastien Binet <binet@cern.ch>
2014-01-14 14:57:59 +0100

    objarray: first import

---
+++ objarray.go
@@ -0,0 +1,31 @@
+package rootio
+
+import (
+	"bytes"
+	"reflect"
+)
+
+type objarray []Object
+
+// ROOTUnmarshaler is the interface implemented by an object that can
+// unmarshal itself from a ROOT buffer
+func (arr *objarray) UnmarshalROOT(data *bytes.Buffer) error {
+	var err error
+	panic("not implemented")
+	return err
+}
+
+func init() {
+	f := func() reflect.Value {
+		o := make(objarray, 0)
+		return reflect.ValueOf(o)
+	}
+	Factory.db["TBranch"] = f
+	Factory.db["*rootio.Branch"] = f
+}
+
+// check interfaces
+//var _ Object = (*objarray)(nil)
+var _ ROOTUnmarshaler = (*objarray)(nil)
+
+// EOF
