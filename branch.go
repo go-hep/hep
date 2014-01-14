14884d4e0da0e30b7d54876840510e755b790462
Sebastien Binet <binet@cern.ch>
2014-01-14 14:56:16 +0100

    branch: first import

---
+++ branch.go
@@ -0,0 +1,62 @@
+package rootio
+
+import (
+	"bytes"
+	"reflect"
+)
+
+type Branch struct {
+	f *File
+
+	named      named
+	autodelete bool
+	branches   []Branch // list of branches of this branch
+	leaves     []Leaf   // list of leaves of this branch
+	baskets    []Basket // list of baskets of this branch
+
+	readbasket  int     // current basket number when reading
+	readentry   int64   // current entry number when reading
+	firstbasket int64   // first entry in the current basket
+	nextbasket  int64   // next entry that will reaquire us to go to the next basket
+	currbasket  *Basket // pointer to the current basket
+
+	tree   *Tree      // tree header
+	mother *Branch    // top-level parent branch in the tree
+	parent *Branch    // parent branch
+	dir    *directory // directory where this branch's buffers are stored
+}
+
+func (b *Branch) Name() string {
+	return b.named.Name()
+}
+
+func (b *Branch) Title() string {
+	return b.named.Title()
+}
+
+func (b *Branch) Class() string {
+	return "TBranch"
+}
+
+// ROOTUnmarshaler is the interface implemented by an object that can
+// unmarshal itself from a ROOT buffer
+func (b *Branch) UnmarshalROOT(data *bytes.Buffer) error {
+	var err error
+	panic("not implemented")
+	return err
+}
+
+func init() {
+	f := func() reflect.Value {
+		o := &Branch{}
+		return reflect.ValueOf(o)
+	}
+	Factory.db["TBranch"] = f
+	Factory.db["*rootio.Branch"] = f
+}
+
+// check interfaces
+var _ Object = (*Branch)(nil)
+var _ ROOTUnmarshaler = (*Branch)(nil)
+
+// EOF
