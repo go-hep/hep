262e9bd54180e0097ae7f6077af8f9780b429064
Sebastien Binet <binet@cern.ch>
2014-03-27 16:08:51 +0100

    p4: extend P4 interface

--- p4.go
+++ p4.go
@@ -1,28 +1,29 @@
 package fmom
 
+// P4 models a Lorentz 4-vector.
 type P4 interface {
-	Px() float64
-	Py() float64
-	Pz() float64
-	E() float64
+	Px() float64       // x component of 4-momentum
+	Py() float64       // y component of 4-momentum
+	Pz() float64       // z component of 4-momentum
+	M() float64        // mass
+	M2() float64       // mass squared
+	P() float64        // momentum magnitude
+	P2() float64       // square of momentum magnitude
+	Eta() float64      // pseudo-rapidity
+	Rapidity() float64 // rapidity
+	Phi() float64      // azimuthal angle in [-pi,pi)
+	E() float64        // energy of 4-momentum
+	Et() float64       // transverse energy defined to be E*sin(Theta)
+	Pt() float64       // transverse momentum
+	IPt() float64      // inverse of transverse momentum
+	CosPhi() float64   // cosine(Phi)
+	SinPhi() float64   // sine(Phi)
+	CosTh() float64    // cosine(Theta)
+	SinTh() float64    // sine(Theta)
+	CotTh() float64    // cottan(Theta)
+	TanTh() float64    // tan(Theta)
 
-	Pt() float64
-	Eta() float64
-	Phi() float64
-}
-
-type ThreeVector [3]float64
-
-func (vec *ThreeVector) X() float64 {
-	return vec[0]
-}
-
-func (vec *ThreeVector) Y() float64 {
-	return vec[1]
-}
-
-func (vec *ThreeVector) Z() float64 {
-	return vec[2]
+	Set(p4 P4)
 }
 
 // EOF
