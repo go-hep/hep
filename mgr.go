d28b867286f545b80e8f9e35239dd2095ef7efe7
Sebastien Binet <binet@cern.ch>
2014-03-31 12:29:37 +0200

    mgr: dont expose sync.Lock. add Get/Has

--- mgr.go
+++ mgr.go
@@ -1,34 +1,56 @@
 package rio
 
-import "sync"
-
-type streamManager struct {
-	sync.RWMutex
-	db map[string]*Stream
-}
-
-type recordManager struct {
-	sync.RWMutex
-	db map[string]*Record
-}
-
-type blockManager struct {
-	sync.RWMutex
-	db map[string]Block
-}
+import (
+	"sync"
+)
 
 var (
-	StreamMgr = streamManager{
+	StreamMgr = streamMgr{
 		db: make(map[string]*Stream),
 	}
 
-	RecordMgr = recordManager{
+	RecordMgr = recordMgr{
 		db: make(map[string]*Record),
 	}
 
-	BlockMgr = blockManager{
+	BlockMgr = blockMgr{
 		db: make(map[string]Block),
 	}
 )
+
+type streamMgr struct {
+	mu sync.RWMutex
+	db map[string]*Stream
+}
+
+type recordMgr struct {
+	mu sync.RWMutex
+	db map[string]*Record
+}
+
+func (mgr *recordMgr) Get(name string) *Record {
+	mgr.mu.RLock()
+	rec, _ := mgr.db[name]
+	mgr.mu.RUnlock()
+	return rec
+}
+
+func (mgr *recordMgr) Has(name string) bool {
+	mgr.mu.RLock()
+	_, ok := mgr.db[name]
+	mgr.mu.RUnlock()
+	return ok
+}
+
+func (mgr *recordMgr) Add(name string) {
+	mgr.mu.Lock()
+	mgr.db[name] = &Record{name: name}
+	mgr.mu.Unlock()
+}
+
+type blockMgr struct {
+	mu sync.RWMutex
+	db map[string]Block
+}
 
 // EOF
