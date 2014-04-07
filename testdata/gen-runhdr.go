886b9253fe2c3afbeecc9cac24bd6f15fa28a467
Sebastien Binet <binet@cern.ch>
2014-04-07 18:12:52 +0200

    gen-runhdr: first import

---
+++ testdata/gen-runhdr.go
@@ -0,0 +1,84 @@
+//+build ignore
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/go-hep/rio"
+)
+
+type RunHeader struct {
+	RunNbr   int32
+	Detector string
+	Descr    string
+	SubDets  []string
+	Ints     []int64
+	Floats   []float64
+}
+
+func main() {
+	fname := flag.String("fname", "runhdr.rio", "file to create")
+	compr := flag.Bool("compr", false, "enable records compression")
+	flag.Parse()
+
+	f, err := rio.Create(*fname)
+	if err != nil {
+		fmt.Printf("*** error: %v\n", err)
+		os.Exit(1)
+	}
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			fmt.Printf("*** error closing file [%s]: %v\n", *fname, err)
+			os.Exit(1)
+		}
+	}()
+
+	var runhdr RunHeader
+	rec := f.Record("RioRunHeader")
+	if rec == nil {
+		fmt.Printf("*** error: could not fetch record [RioRunHeader]\n")
+		os.Exit(1)
+	}
+
+	rec.SetCompress(*compr)
+	err = rec.Connect("RunHeader", &runhdr)
+	if err != nil {
+		fmt.Printf("error connecting [RunHeader]: %v", err)
+		os.Exit(1)
+	}
+
+	for irec := 0; irec < 10; irec++ {
+		runhdr = RunHeader{
+			RunNbr:   int32(irec),
+			Detector: "MyDetector",
+			Descr:    "dummy run number",
+			SubDets:  []string{"subdet 0", "subdet 1"},
+			Floats: []float64{
+				float64(irec) + 100,
+				float64(irec) + 200,
+				float64(irec) + 300,
+			},
+			Ints: []int64{
+				int64(irec) + 100,
+				int64(irec) + 200,
+				int64(irec) + 300,
+			},
+		}
+		err = f.WriteRecord(rec)
+		if err != nil {
+			fmt.Printf("error writing record: %v (irec=%d)", err, irec)
+			os.Exit(1)
+		}
+
+		err = f.Sync()
+		if err != nil {
+			fmt.Printf("error flushing record: %v (irec=%d)", err, irec)
+			os.Exit(1)
+		}
+	}
+}
+
+// EOF
