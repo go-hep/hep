375fc1f2ec4c38d0ea153716b224bf00c60ad426
Sebastien Binet <binet@cern.ch>
2014-04-14 15:11:45 +0200

    errors: add ErrBlockConnected

--- errors.go
+++ errors.go
@@ -7,6 +7,7 @@ import (
 var (
 	ErrStreamNoRecMarker   = errors.New("rio: no record marker found")
 	ErrRecordNoBlockMarker = errors.New("rio: no block marker found")
+	ErrBlockConnected      = errors.New("rio: block already connected")
 )
 
 // EOF
