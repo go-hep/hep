69ed80a396540272bcb0a14a30a1f0da8cdafdc8
Sebastien Binet <binet@cern.ch>
2014-04-17 18:10:24 +0200

    dflow: more dataflow consistency checks (type, in/out)

--- dflow.go
+++ dflow.go
@@ -1,30 +1,32 @@
 package fwk
 
 import (
+	"bytes"
+	"fmt"
 	"reflect"
 )
 
 type node struct {
-	in  map[string]struct{}
-	out map[string]struct{}
+	in  map[string]reflect.Type
+	out map[string]reflect.Type
 }
 
 func newNode() *node {
 	return &node{
-		in:  make(map[string]struct{}),
-		out: make(map[string]struct{}),
+		in:  make(map[string]reflect.Type),
+		out: make(map[string]reflect.Type),
 	}
 }
 
 type dflowsvc struct {
 	SvcBase
 	nodes map[string]*node
-	edges map[string]struct{}
+	edges map[string]reflect.Type
 }
 
 func (svc *dflowsvc) Configure(ctx Context) Error {
 	svc.nodes = make(map[string]*node)
-	svc.edges = make(map[string]struct{})
+	svc.edges = make(map[string]reflect.Type)
 	return nil
 }
 
@@ -45,14 +47,59 @@ func (svc *dflowsvc) addInNode(tsk string, name string, t reflect.Type) Error {
 	_, ok = node.in[name]
 	if ok {
 		return Errorf(
-			"fwk.DeclInPort: component [%s] already declare in-port with name [%s]",
+			"fwk.DeclInPort: component [%s] already declared in-port with name [%s]",
 			tsk,
 			name,
 		)
 	}
 
-	node.in[name] = struct{}{}
-	svc.edges[name] = struct{}{}
+	node.in[name] = t
+	edgetyp, dup := svc.edges[name]
+	if dup {
+		// make sure types match
+		if edgetyp != t {
+			type elem_t struct {
+				port string // in/out
+				task string // task which defined the port
+				typ  reflect.Type
+			}
+			cont := []elem_t{}
+			for tskname, node := range svc.nodes {
+				for k, in := range node.in {
+					if k != name {
+						continue
+					}
+					cont = append(cont,
+						elem_t{
+							port: "in ",
+							task: tskname,
+							typ:  in,
+						},
+					)
+				}
+				for k, out := range node.out {
+					if k != name {
+						continue
+					}
+					cont = append(cont,
+						elem_t{
+							port: "out",
+							task: tskname,
+							typ:  out,
+						},
+					)
+				}
+			}
+			var o bytes.Buffer
+			fmt.Fprintf(&o, "fwk.DeclInPort: detected type inconsistency for port [%s]:\n", name)
+			for _, c := range cont {
+				fmt.Fprintf(&o, " component=%q port=%s type=%v\n", c.task, c.port, c.typ)
+			}
+			return fmt.Errorf(string(o.Bytes()))
+		}
+	}
+
+	svc.edges[name] = t
 	return nil
 }
 
@@ -65,14 +112,38 @@ func (svc *dflowsvc) addOutNode(tsk string, name string, t reflect.Type) Error {
 	_, ok = node.out[name]
 	if ok {
 		return Errorf(
-			"fwk.DeclInPort: component [%s] already declare out-port with name [%s]",
+			"fwk.DeclOutPort: component [%s] already declared out-port with name [%s]",
 			tsk,
 			name,
 		)
 	}
 
-	node.out[name] = struct{}{}
-	svc.edges[name] = struct{}{}
+	node.out[name] = t
+
+	edgetyp, dup := svc.edges[name]
+	if dup {
+		// edge already exists
+		// loop over nodes, find out who already defined that edge
+		for duptsk, dupnode := range svc.nodes {
+			if duptsk == tsk {
+				continue
+			}
+			for out := range dupnode.out {
+				if out == name {
+					return Errorf(
+						"fwk.DeclOutPort: component [%s] already declared out-port with name [%s (type=%v)].\nfwk.DeclOutPort: component [%s] is trying to add a duplicate out-port [%s (type=%v)]",
+						duptsk,
+						name,
+						edgetyp,
+						tsk,
+						name,
+						t,
+					)
+				}
+			}
+		}
+	}
+	svc.edges[name] = t
 	return nil
 }
 
