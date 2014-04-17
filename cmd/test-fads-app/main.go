125be75e2642565ab041f977f1bf2fae68cdd24a
Sebastien Binet <binet@cern.ch>
2014-04-17 10:52:19 +0200

    Revert "test-fads-app: run hepmcreader" need some big-ish data file...
    This reverts commit ff340c9a207ce2e380275bea31c7c84bd4ae12b9.

--- cmd/test-fads-app/main.go
+++ cmd/test-fads-app/main.go
@@ -35,12 +35,6 @@ func main() {
 	}
 	mgr.AddTask(c.(fwk.Task))
 
-	c, err = fwk.New("github.com/go-hep/fads.HepMcReader", "hepmcreader")
-	if err != nil {
-		panic(err)
-	}
-	mgr.AddTask(c.(fwk.Task))
-
 	err = app.Run()
 	if err != nil {
 		panic(err)
