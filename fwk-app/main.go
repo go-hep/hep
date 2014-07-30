49f06a364d408b1c81616f5b9cf743d5ab3a1d52
Sebastien Binet <binet@cern.ch>
2014-07-30 15:30:32 +0200

    fwk-app: first stab at a fwk driver

---
+++ fwk-app/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-hep/fwk"
+	"github.com/go-hep/fwk/job"
+)
+
+func handle_err(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Printf("::: fwk-app...\n")
+
+	app := job.New(fwk.NewApp())
+
+	app.Create(job.C{
+		Type: "github.com/go-hep/fads.ParticlePropagator",
+		Name: "pprop",
+	})
+
+	app.Create(job.C{
+		Type: "github.com/go-hep/fads.HepMcReader",
+		Name: "hepmcreader",
+		Props: job.P{
+			"Input": "testdata/hepmc.data",
+		},
+	})
+
+	app.Create(job.C{
+		Type: "github.com/go-hep/fads.Efficiency",
+		Name: "charged-hadron-trk-eff",
+		Props: job.P{
+			"Input": "ChargedHadrons",
+		},
+	})
+
+	app.Run()
+
+	fmt.Printf("::: fwk-app... [done]\n")
+}
