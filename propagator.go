7ea80dd0b5bca6a60f71c7f361aae976acee625e
Sebastien Binet <binet@cern.ch>
2014-07-30 15:38:30 +0200

    propagator: implement particle propagation

--- propagator.go
+++ propagator.go
@@ -1,8 +1,10 @@
 package fads
 
 import (
+	"math"
 	"reflect"
 
+	"github.com/go-hep/fmom"
 	"github.com/go-hep/fwk"
 )
 
@@ -109,6 +111,202 @@ func (tsk *ParticlePropagator) Process(ctx fwk.Context) fwk.Error {
 		}
 	}()
 
+	const (
+		cLight  = 2.99792458E8
+		cLight2 = cLight * cLight
+	)
+
+	for i := range input {
+		cand := &input[i]
+		x := cand.Pos.X() * 1e-3
+		y := cand.Pos.Y() * 1e-3
+		z := cand.Pos.Z() * 1e-3
+
+		// is particle inside cylinder ?
+		if math.Hypot(x, y) > tsk.radius || math.Abs(z) > tsk.halflen {
+			continue
+		}
+
+		px := cand.Mom.Px()
+		py := cand.Mom.Py()
+		pt2 := px*px + py*py
+		if pt2 < 1e-9 {
+			continue
+		}
+
+		q := float64(cand.Charge())
+		if math.Abs(q) < 1e-9 || math.Abs(tsk.bz) < 1e-9 {
+			// solve pt2*t^2 + 2(px.x + py.y)*t + (radius2 - x.x - y.y) = 0
+			v := px*y - py*x
+			discr2 := pt2*tsk.radius2 - v*v
+			if discr2 < 0 {
+				// no solution
+				continue
+			}
+
+			v = px*x + py*y
+			discr := math.Sqrt(discr2)
+			t1 := (-v + discr) / pt2
+			t2 := (-v - discr) / pt2
+			t := 0.0
+			switch t1 < 0 {
+			case true:
+				t = t2
+			case false:
+				t = t1
+			}
+
+			pz := cand.Mom.Pz()
+			e := cand.Mom.E()
+
+			xt := x + px*t
+			yt := y + py*t
+			zt := z + pz*t
+
+			mother := cand
+			c := cand.Clone()
+			c.Pos = fmom.NewPxPyPzE(xt*1e3, yt*1e3, zt*1e3, cand.Pos.T()+t*e*1e3)
+			c.Add(mother)
+
+			output = append(output, *c)
+			if math.Abs(q) > 1e-9 {
+				pid := c.Pid
+				if pid < 0 {
+					pid = 0
+				}
+				switch pid {
+				case 11:
+					eles = append(eles, *c)
+				case 13:
+					muons = append(muons, *c)
+				default:
+					hadrons = append(hadrons, *c)
+				}
+			}
+		} else {
+			// 1.  initial transverse momentum p_{T0} : Part->pt
+			//     initial transverse momentum direction \phi_0 = -atan(p_X0/p_Y0)
+			//     relativistic gamma : gamma = E/mc² ; gammam = gamma \times m
+			//     giration frequency \omega = q/(gamma m) fBz
+			//     helix radius r = p_T0 / (omega gamma m)
+
+			e := cand.Mom.E()
+			pt := cand.Mom.Pt()
+			pz := cand.Mom.Pz()
+
+			gammam := e * 1e9 / cLight2  // in eV/c^2
+			omega := q * tsk.bz / gammam // omega is in [89875518 / s]
+			r := pt / (q * tsk.bz) * 1e9 / cLight
+
+			phi0 := math.Atan2(py, px) // [rad] in [-pi,pi)
+
+			// 2. helix axis coordinates
+			xc := x + r*math.Sin(phi0)
+			yc := y - r*math.Cos(phi0)
+			rc := math.Hypot(xc, yc)
+			phic := math.Atan2(yc, xc)
+			phi := phic
+			if xc < 0 {
+				phi += math.Pi
+			}
+
+			// 3. time evaluation t = TMath::Min(t_r, t_z)
+			//    t_r : time to exit from the sides
+			//    t_z : time to exit from the front or the back
+			tr := 0.0 // in [ns]
+			signpz := 1.0
+			if math.Signbit(pz) {
+				signpz = -1.0
+			}
+
+			tz := 0.0
+			if pz == 0 {
+				tz = 1e99
+			} else {
+				tz = gammam / (pz * 1e9 / cLight) * (-z * tsk.halflen * signpz)
+			}
+
+			absr := math.Abs(r)
+			t := 0.0
+			if rc+absr < tsk.radius {
+				// helix does not cross the cylinder sides
+				t = tz
+			} else {
+				asinrho := math.Asin((tsk.radius*tsk.radius - rc*rc - r*r) * 0.5 / (absr * rc))
+				delta := phi0 - phi
+				if delta < -math.Pi {
+					delta += 2.0 * math.Pi
+				}
+				if delta > math.Pi {
+					delta -= 2.0 * math.Pi
+				}
+
+				t1 := (delta + asinrho) / omega
+				t2 := (delta + math.Pi - asinrho) / omega
+				t3 := (delta + math.Pi + asinrho) / omega
+				t4 := (delta - asinrho) / omega
+				t5 := (delta - math.Pi - asinrho) / omega
+				t6 := (delta - math.Pi + asinrho) / omega
+
+				if t1 < 0 {
+					t1 = 1.0E99
+				}
+				if t2 < 0 {
+					t2 = 1.0E99
+				}
+				if t3 < 0 {
+					t3 = 1.0E99
+				}
+				if t4 < 0 {
+					t4 = 1.0E99
+				}
+				if t5 < 0 {
+					t5 = 1.0E99
+				}
+				if t6 < 0 {
+					t6 = 1.0E99
+				}
+
+				tra := math.Min(t1, math.Min(t2, t3))
+				trb := math.Min(t4, math.Min(t5, t6))
+				tr = math.Min(tra, trb)
+				t = math.Min(tr, tz)
+			}
+
+			// 4. position in terms of x(t), y(t), z(t)
+			xt := xc + r*math.Sin(omega*t-phi0)
+			yt := yc + r*math.Cos(omega*t-phi0)
+			zt := z + pz*1.0E9/cLight/gammam*t
+			rt := math.Hypot(xt, yt)
+
+			if rt > 0.0 {
+				mother := cand
+				c := cand.Clone()
+				c.Pos = fmom.NewPxPyPzE(xt*1e3, yt*1e3, zt*1e3, cand.Pos.T()+t*e*1e3)
+				c.Add(mother)
+
+				output = append(output, *c)
+				if math.Abs(q) > 1e-9 {
+					pid := c.Pid
+					if pid < 0 {
+						pid = 0
+					}
+					switch pid {
+					case 11:
+						eles = append(eles, *c)
+					case 13:
+						muons = append(muons, *c)
+					default:
+						hadrons = append(hadrons, *c)
+					}
+				}
+
+			}
+
+		}
+	}
+
+	msg.Infof(">>> output:     %v\n", len(output))
 	return err
 }
 
