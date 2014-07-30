084ad97cf83b2e0342b16a11db3371544746ca8a
Sebastien Binet <binet@cern.ch>
2014-07-30 15:11:03 +0200

    base: remove global g_mgr. removed obsolete Base

--- base.go
+++ base.go
@@ -4,20 +4,16 @@ import (
 	"reflect"
 )
 
-type Base struct {
-	name string
-}
-
-func (c Base) Name() string {
-	return c.name
-}
-
-func (c *Base) SetName(n string) {
-	c.name = n
-}
-
 type TaskBase struct {
 	name string
+	mgr  App
+}
+
+func NewTask(name string, mgr App) TaskBase {
+	return TaskBase{
+		name: name,
+		mgr:  mgr,
+	}
 }
 
 func (tsk TaskBase) Name() string {
@@ -28,43 +24,36 @@ func (tsk *TaskBase) SetName(n string) {
 	tsk.name = n
 }
 
-func (tsk TaskBase) DeclInPort(name string, t reflect.Type) Error {
-	return g_mgr.dflow.addInNode(tsk.name, name, t)
+func (tsk *TaskBase) DeclInPort(name string, t reflect.Type) Error {
+	return tsk.mgr.DeclInPort(tsk, name, t)
 }
 
-func (tsk TaskBase) DeclOutPort(name string, t reflect.Type) Error {
-	return g_mgr.dflow.addOutNode(tsk.name, name, t)
+func (tsk *TaskBase) DeclOutPort(name string, t reflect.Type) Error {
+	return tsk.mgr.DeclOutPort(tsk, name, t)
 }
 
-func (tsk TaskBase) DeclProp(name string, ptr interface{}) Error {
-	c := g_mgr.GetTask(tsk.name)
-	if c == nil {
-		return Errorf("fwk.DeclProp: no Task [%s] known to TaskMgr", tsk.name)
-	}
-
-	return g_mgr.DeclProp(c, name, ptr)
+func (tsk *TaskBase) DeclProp(name string, ptr interface{}) Error {
+	return tsk.mgr.DeclProp(tsk, name, ptr)
 }
 
-func (tsk TaskBase) SetProp(name string, value interface{}) Error {
-	c := g_mgr.GetTask(tsk.name)
-	if c == nil {
-		return Errorf("fwk.SetProp: no Task [%s] known to TaskMgr", tsk.name)
-	}
-
-	return g_mgr.SetProp(c, name, value)
+func (tsk *TaskBase) SetProp(name string, value interface{}) Error {
+	return tsk.mgr.SetProp(tsk, name, value)
 }
 
-func (tsk TaskBase) GetProp(name string) (interface{}, Error) {
-	c := g_mgr.GetTask(tsk.name)
-	if c == nil {
-		return nil, Errorf("fwk.GetProp: no Task [%s] known to TaskMgr", tsk.name)
-	}
-
-	return g_mgr.GetProp(c, name)
+func (tsk *TaskBase) GetProp(name string) (interface{}, Error) {
+	return tsk.mgr.GetProp(tsk, name)
 }
 
 type SvcBase struct {
 	name string
+	mgr  App
+}
+
+func NewSvc(name string, mgr App) SvcBase {
+	return SvcBase{
+		name: name,
+		mgr:  mgr,
+	}
 }
 
 func (svc SvcBase) Name() string {
@@ -75,31 +64,16 @@ func (svc *SvcBase) SetName(n string) {
 	svc.name = n
 }
 
-func (svc SvcBase) DeclProp(name string, ptr interface{}) Error {
-	c := g_mgr.GetSvc(svc.name)
-	if c == nil {
-		return Errorf("fwk.DeclProp: no Svc [%s] known to SvcMgr", name)
-	}
-
-	return g_mgr.DeclProp(c, name, ptr)
+func (svc *SvcBase) DeclProp(name string, ptr interface{}) Error {
+	return svc.mgr.DeclProp(svc, name, ptr)
 }
 
-func (svc SvcBase) SetProp(name string, value interface{}) Error {
-	c := g_mgr.GetSvc(svc.name)
-	if c == nil {
-		return Errorf("fwk.SetProp: no Svc [%s] known to SvcMgr", name)
-	}
-
-	return g_mgr.SetProp(c, name, value)
+func (svc *SvcBase) SetProp(name string, value interface{}) Error {
+	return svc.mgr.SetProp(svc, name, value)
 }
 
-func (svc SvcBase) GetProp(name string) (interface{}, Error) {
-	c := g_mgr.GetSvc(svc.name)
-	if c == nil {
-		return nil, Errorf("fwk.GetProp: no Svc [%s] known to SvcMgr", name)
-	}
-
-	return g_mgr.GetProp(c, name)
+func (svc *SvcBase) GetProp(name string) (interface{}, Error) {
+	return svc.mgr.GetProp(svc, name)
 }
 
 // EOF
