c609d0373ea1b53c94b4a7cb34bc1635eea99a22
Sebastien Binet <binet@cern.ch>
2014-08-01 11:56:37 +0200

    job/utils: first import

---
+++ job/utils.go
@@ -0,0 +1,24 @@
+package job
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-hep/fwk"
+)
+
+func MsgLevel(lvl string) fwk.Level {
+	switch strings.ToUpper(lvl) {
+	case "DEBUG":
+		return fwk.LvlDebug
+	case "INFO":
+		return fwk.LvlInfo
+	case "WARNING":
+		return fwk.LvlWarning
+	case "ERROR":
+		return fwk.LvlError
+	default:
+		panic(fmt.Errorf("fwk.MsgLevel: invalid fwk.Level string %q", lvl))
+	}
+	return fwk.LvlInfo
+}
