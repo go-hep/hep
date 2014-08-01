6d6a7c60428668f686646668ed6be2ac7b7347c0
Sebastien Binet <binet@cern.ch>
2014-08-01 18:47:02 +0200

    calo: first real stab

--- calo.go
+++ calo.go
@@ -1,34 +1,149 @@
 package fads
 
 import (
+	"reflect"
+
 	"github.com/go-hep/fwk"
 )
+
+type EtaPhiBin struct {
+	EtaBins []float64
+	PhiBins []float64
+}
+
+type EneFrac struct {
+	ECal float64
+	HCal float64
+}
 
 type Calorimeter struct {
 	fwk.TaskBase
 
-	fracmap map[int64][2]float64
+	fracmap map[int]EneFrac
 	binmap  map[float64]map[float64]struct{}
 
-	ecalres func(float64) float64
-	hcalres func(float64) float64
+	etaphibins []EtaPhiBin
+	ecalres    func(eta, ene float64) float64
+	hcalres    func(eta, ene float64) float64
 
-	tower *Candidate
+	particles   string
+	tracks      string
+	towers      string
+	photons     string
+	eflowtracks string
+	eflowtowers string
 }
 
-func (proc *Calorimeter) StartTask(ctx fwk.Context) fwk.Error {
+func (tsk *Calorimeter) Configure(ctx fwk.Context) fwk.Error {
 	var err fwk.Error
+
+	// err = tsk.DeclInPort(tsk.input, reflect.TypeOf(sometype{}))
+	// if err != nil {
+	//	return err
+	// }
+
+	// err = tsk.DeclOutPort(tsk.output, reflect.TypeOf(sometype{}))
+	// if err != nil {
+	//	return err
+	// }
+
 	return err
 }
 
-func (proc *Calorimeter) Process(ctx fwk.Context) fwk.Error {
+func (tsk *Calorimeter) StartTask(ctx fwk.Context) fwk.Error {
 	var err fwk.Error
+
 	return err
 }
 
-func (proc *Calorimeter) StopTask(ctx fwk.Context) fwk.Error {
+func (tsk *Calorimeter) StopTask(ctx fwk.Context) fwk.Error {
 	var err fwk.Error
+
 	return err
 }
 
-// EOF
+func (tsk *Calorimeter) Process(ctx fwk.Context) fwk.Error {
+	var err fwk.Error
+
+	return err
+}
+
+func newCalorimeter(typ, name string, mgr fwk.App) (fwk.Component, fwk.Error) {
+	var err fwk.Error
+
+	tsk := &Calorimeter{
+		TaskBase: fwk.NewTask(typ, name, mgr),
+
+		fracmap:    make(map[int]EneFrac),
+		etaphibins: make([]EtaPhiBin, 0, 2),
+		ecalres:    func(eta, ene float64) float64 { return 0 },
+		hcalres:    func(eta, ene float64) float64 { return 0 },
+
+		particles:   "/fads/particles",
+		tracks:      "/fads/tracks",
+		towers:      "/fads/towers",
+		photons:     "/fads/photons",
+		eflowtracks: "/fads/eflowtracks",
+		eflowtowers: "/fads/eflowtowers",
+	}
+
+	// --
+
+	err = tsk.DeclProp("EtaPhiBins", &tsk.etaphibins)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("EnergyFraction", &tsk.fracmap)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("ECalResolution", &tsk.ecalres)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("HCalResolution", &tsk.hcalres)
+	if err != nil {
+		return nil, err
+	}
+
+	// --
+
+	err = tsk.DeclProp("Particles", &tsk.particles)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("Tracks", &tsk.tracks)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("Towers", &tsk.towers)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("Photons", &tsk.photons)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("EFlowTracks", &tsk.eflowtracks)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tsk.DeclProp("EFlowTowers", &tsk.eflowtowers)
+	if err != nil {
+		return nil, err
+	}
+
+	return tsk, err
+}
+
+func init() {
+	fwk.Register(reflect.TypeOf(Calorimeter{}), newCalorimeter)
+}
