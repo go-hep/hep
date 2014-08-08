d8d684e856f4b24fb97641a932046a805e1d48ad
Sebastien Binet <binet@cern.ch>
2014-08-08 18:40:53 +0200

    pseudojet: add UserInfo

--- pseudojet.go
+++ pseudojet.go
@@ -4,7 +4,12 @@ import (
 	"github.com/go-hep/fmom"
 )
 
+// UserInfo holds extra user information in a PseudoJet
+type UserInfo interface{}
+
 // PseudoJet holds minimal information of use for jet-clustering routines
 type PseudoJet struct {
 	fmom.PxPyPzE
+
+	UserInfo UserInfo
 }
