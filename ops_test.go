273ca574e00ba48ac0f5599a3d8f82302bc7ded4
Sebastien Binet <binet@cern.ch>
2014-08-12 16:01:06 +0200

    fmom: add TestEqual

--- ops_test.go
+++ ops_test.go
@@ -1,7 +1,6 @@
 package fmom
 
 import (
-	"math"
 	"reflect"
 	"testing"
 )
@@ -35,20 +34,7 @@ func newIPtCotThPhiM(p4 PxPyPzE) P4 {
 }
 
 func deepEqual(p1, p2 P4) bool {
-	const epsilon = 1e-6
-	if math.Abs(p1.Px()-p2.Px()) > epsilon {
-		return false
-	}
-	if math.Abs(p1.Py()-p2.Py()) > epsilon {
-		return false
-	}
-	if math.Abs(p1.Pz()-p2.Pz()) > epsilon {
-		return false
-	}
-	if math.Abs(p1.E()-p2.E()) > epsilon {
-		return false
-	}
-	return true
+	return Equal(p1, p2)
 }
 
 func TestAdd(t *testing.T) {
@@ -147,6 +133,72 @@ func TestIAdd(t *testing.T) {
 		}
 		if !reflect.DeepEqual(p2, table.p2) {
 			t.Fatalf("fmom.IAdd modified p2:\np1=%#v (ref)\np2=%#v (new)", table.p2, p2)
+		}
+	}
+}
+
+func TestEqual(t *testing.T) {
+	for _, table := range []struct {
+		p1  P4
+		p2  P4
+		exp bool
+	}{
+		{
+			p1:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: true,
+		},
+		{
+			p1:  newEEtaPhiM(NewPxPyPzE(10, 10, 10, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: true,
+		},
+		{
+			p1:  newEtEtaPhiM(NewPxPyPzE(10, 10, 10, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: true,
+		},
+		{
+			p1:  newPtEtaPhiM(NewPxPyPzE(10, 10, 10, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: true,
+		},
+		{
+			p1:  newIPtCotThPhiM(NewPxPyPzE(10, 10, 10, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: true,
+		},
+
+		{
+			p1:  newPxPyPzE(NewPxPyPzE(10+1e-14, 10, 10, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: false,
+		},
+		{
+			p1:  newPxPyPzE(NewPxPyPzE(10, 10+1e-14, 10, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: false,
+		},
+		{
+			p1:  newPxPyPzE(NewPxPyPzE(10, 10, 10+1e-14, 20)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: false,
+		},
+		{
+			p1:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20+1e-14)),
+			p2:  newPxPyPzE(NewPxPyPzE(10, 10, 10, 20)),
+			exp: false,
+		},
+	} {
+		{
+			eq := deepEqual(table.p1, table.p2)
+			if eq != table.exp {
+				t.Fatalf("exp: %#v\ngot: %#v\np1=%#v\np2=%#v\n", table.exp, eq, table.p1, table.p2)
+			}
+		}
+		eq := Equal(table.p1, table.p2)
+		if eq != table.exp {
+			t.Fatalf("exp: %#v\ngot: %#v\np1=%#v\np2=%#v\n", table.exp, eq, table.p1, table.p2)
 		}
 	}
 }
