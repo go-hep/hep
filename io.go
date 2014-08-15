09d0170a15bb7a44450ea915a7a7e290f23f046a
Sebastien Binet <binet@cern.ch>
2014-08-15 17:52:42 +0200

    io: first stab

--- io.go
+++ io.go
@@ -1,6 +1,15 @@
 package fwk
 
-type IOStreamer interface {
+// InputStreamer reads data from the underlying io.Reader
+// and puts it into fwk's Context
+type InputStreamer interface {
+	Read(ctx Context) error
+}
+
+// OutputStreamer gets data from the Context
+// and writes it to the underlying io.Writer
+type OutputStreamer interface {
+	Write(ctx Context) error
 }
 
 // EOF
