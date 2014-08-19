2fed05ee90d421da4efe41961352b763d17332af
Sebastien Binet <binet@cern.ch>
2014-08-19 17:43:27 +0200

    doc: some more guidelines

--- fwk.go
+++ fwk.go
@@ -11,6 +11,27 @@
 // whiteboard/event-store service, a data-flow service, ...) but do not
 // typically take (directly) part of the event processing.
 //
+// Typically, users will implement fwk.Tasks, ie:
+//
+//   type MyTask struct {
+//     fwk.TaskBase
+//   }
+//
+//   // Configure is called once, after having read the properties
+//   // from the data-cards.
+//   func (tsk *MyTask) Configure(ctx fwk.Context) error { return nil }
+//
+//   // StartTask is called once (sequentially), just before
+//   // the main event-loop processing.
+//   func (tsk *MyTask) StartTask(ctx fwk.Context) error { return nil }
+//
+//   // Process is called for each event, (quite) possibly concurrently.
+//   func (tsk *MyTask) Process(ctx fwk.Context)   error { return nil }
+//
+//   // StopTask is called once (sequentially), just after the
+//   // main event-loop processing finished.
+//   func (tsk *MyTask) StopTask(ctx fwk.Context)  error { return nil }
+//
 // A fwk application processes data concurrently, a 2 different levels:
 //  - event-level concurrency: multiple events are processed concurrently
 //    at any given time, during the event loop;
@@ -33,4 +54,30 @@
 //  err = component.DeclInPort("Electrons", t)
 //  err = component.DeclOutPort("ReScaledElectrons", t)
 //
+// Then, during the event processing, one gets and puts data from/to the store
+// like so:
+//
+//   func (tsk *MyTask) Process(ctx fwk.Context) error {
+//      var err error
+//
+//      // retrieve store associated with this event / region-of-interest
+//      store := ctx.Store()
+//      v, err := store.Get("Electrons")
+//      if err != nil {
+//         return err
+//      }
+//      eles := v.([]Electron) // type-cast to correct type
+//
+//      // create output collection
+//      out := make([]Electron, 0, len(eles))
+//
+//      // make sure the collection be put in the store
+//      defer func() {
+//         err = store.Put("ReScaledElectrons", out)
+//      }()
+//
+//      // ... do some massaging with 'eles' and 'out'
+//
+//      return err
+//   }
 package fwk
