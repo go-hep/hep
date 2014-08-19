beeb6a7500c91905ddbf0dc984dd35631d11f694
Sebastien Binet <binet@cern.ch>
2014-08-19 15:25:56 +0200

    doc: first stab

---
+++ fwk.go
@@ -0,0 +1,4 @@
+// fwk provides a set of tools to process High Energy Physics events data.
+//
+// fwk is a components-based framework, a-la Gaudi, with builtin support for concurrency.
+package fwk
