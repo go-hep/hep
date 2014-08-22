1147797e41ed8253d388b83978c53fe44d08aef1
Sebastien Binet <binet@cern.ch>
2014-08-22 12:02:29 +0200

    error: use errstack

--- error.go
+++ error.go
@@ -1,13 +1,18 @@
 package fwk
 
 import (
-	"fmt"
+	"github.com/go-hep/fwk/utils/errstack"
 )
 
 // Errorf formats according to a format specifier and returns the string as
-// a value that satisfies error.
+// a value that satisfies error, together with the associated stack trace.
 func Errorf(format string, args ...interface{}) error {
-	return fmt.Errorf(format, args...)
+	return errstack.Newf(format, args...)
+}
+
+// Error returns the original error with the associated stack trace.
+func Error(err error) error {
+	return errstack.New(err)
 }
 
 // EOF
