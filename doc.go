e04d44653726f23cf8ed0af0fb2c155ba1e57f88
Sebastien Binet <binet@cern.ch>
2016-02-03 10:28:32 +0100

    hbook: reflow doc + golint

--- doc.go
+++ doc.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// hbook is a set of data analysis tools for HEP (histograms (1D, 2D, 3D), profiles and ntuples).
-// hbook is a work in progress of a concurrent friendly histogram filling toolkit. It is loosely based on AIDA interfaces and concepts as well as the "simplicity" of HBOOK and the previous work of YODA.
+// Package hbook is a set of data analysis tools for HEP (histograms (1D, 2D, 3D),
+// profiles and ntuples).
+// hbook is a work in progress of a concurrent friendly histogram filling toolkit.
+// It is loosely based on AIDA interfaces and concepts as well as the "simplicity"
+// of HBOOK and the previous work of YODA.
 package hbook
