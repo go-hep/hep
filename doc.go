7beef7607346e8ce5e14427073bb97a57c822dbe
Sebastien Binet <binet@cern.ch>
2016-04-05 10:03:00 +0200

    all: add copyright headers

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright ©2016 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // hplot is a package to plot histograms, n-tuples and functions
 package hplot
 
--- hplot.go
+++ hplot.go
@@ -1,3 +1,7 @@
+// Copyright ©2016 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Copyright 2012 The Plotinum Authors. All rights reserved.
 // Use of this source code is governed by an MIT-style license
 // that can be found in the LICENSE file.
