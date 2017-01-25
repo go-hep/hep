b76900616a42d346c7491528ae3f85d130ad82ac
Sebastien Binet <binet@cern.ch>
2017-01-25 10:59:24 +0100

    hepevt: prepare for vanity import path

--- fhepevt/doc.go
+++ fhepevt/doc.go
@@ -1,6 +1,8 @@
-// fhepevt provides r/w access to the FORTRAN-77 HEPEVT common block
-//
-// 
-package fhepevt
+// Copyright 2017 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package fhepevt provides r/w access to the FORTRAN-77 HEPEVT common block
+package fhepevt // import "github.com/go-hep/hepevt/fhepevt"
 
 // EOF
