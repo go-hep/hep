4d0750bd643b0d7f8f108750bb7b996d497308f4
Sebastien Binet <binet@cern.ch>
2017-01-25 11:12:48 +0100

    sio: prepare for vanity import path

--- doc.go
+++ doc.go
@@ -1,2 +1,6 @@
-// sio is a record-oriented persistency mechanism.
-package sio
+// Copyright 2017 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package sio implements a record-oriented persistency mechanism.
+package sio // import "github.com/go-hep/sio"
