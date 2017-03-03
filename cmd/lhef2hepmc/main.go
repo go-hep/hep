232ecedefa7dfe626fb9cd352156d78e27f38d6d
Sebastien Binet <binet@cern.ch>
2017-03-03 15:49:31 +0100

    cmd/lhef2hepmc: move lhef2hepmc under cmd

--- cmd/lhef2hepmc/main.go
+++ cmd/lhef2hepmc/main.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main // import "go-hep.org/x/hep/lhef2hepmc"
+package main // import "go-hep.org/x/hep/cmd/lhef2hepmc"
 
 import (
 	"flag"
