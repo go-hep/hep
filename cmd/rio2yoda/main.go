0b178746bf76cda2f74161532ae3b6a5545071a7
Sebastien Binet <binet@cern.ch>
2017-03-03 15:46:26 +0100

    cmd/rio2yoda: first import
    This CL adds a new top-level command, rio2yoda.
    rio2yoda converts rio files containing hbook values (H1D, H2D, P1D, ...)
    into YODA files.

---
+++ cmd/rio2yoda/main.go
@@ -0,0 +1,142 @@
+// Copyright 2017 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// rio2yoda converts rio files containing hbook values (H1D, H2D, P1D, ...)
+// into YODA files.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+
+	"go-hep.org/x/hep/hbook"
+	"go-hep.org/x/hep/rio"
+)
+
+func main() {
+
+	log.SetFlags(0)
+	log.SetPrefix("rio2yoda: ")
+
+	flag.Usage = func() {
+		fmt.Fprintf(
+			os.Stderr,
+			`Usage: rio2yoda [options] <file.rio> [<file2.rio> [...]]
+	
+ex:
+ $ rio2yoda rivet.rio >| rivet.yoda
+ `)
+	}
+
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		log.Printf("missing input file name")
+		flag.Usage()
+		flag.PrintDefaults()
+	}
+
+	for _, fname := range flag.Args() {
+		convert(fname)
+	}
+}
+
+func convert(fname string) {
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	r, err := rio.Open(f)
+	if err != nil {
+		log.Fatalf("error opening rio stream %q: %v\n", fname, err)
+	}
+	defer r.Close()
+
+	for _, key := range r.Keys() {
+		rt := typeFrom(key.Blocks[0].Type)
+		if rt == nil {
+			continue
+		}
+		v := reflect.New(rt.Elem())
+		err = r.Get(key.Name, v.Interface())
+		if err != nil {
+			log.Fatalf(
+				"error reading block %q from file %q: %v\n",
+				key.Name, fname, err,
+			)
+		}
+
+		yoda, err := v.Interface().(yodaMarshaler).MarshalYODA()
+		if err != nil {
+			log.Fatalf(
+				"error marshaling block %q from file %q to YODA: %v\n",
+				key.Name, fname, err,
+			)
+		}
+
+		_, err = os.Stdout.Write(yoda)
+		if err != nil {
+			log.Fatalf(
+				"error streaming out YODA format for block %q from file %q: %v\n",
+				key.Name, fname, err,
+			)
+		}
+
+	}
+}
+
+func nameFromType(rt reflect.Type) string {
+	if rt == nil {
+		return "interface"
+	}
+	// Default to printed representation for unnamed types
+	name := rt.String()
+
+	// But for named types (or pointers to them), qualify with import path.
+	// Dereference one pointer looking for a named type.
+	star := ""
+	if rt.Name() == "" {
+		pt := rt
+		if pt.Kind() == reflect.Ptr {
+			star = "*"
+			rt = pt.Elem()
+		}
+	}
+
+	if rt.Name() != "" {
+		switch rt.PkgPath() {
+		case "":
+			name = star + rt.Name()
+		default:
+			name = star + rt.PkgPath() + "." + rt.Name()
+		}
+	}
+
+	return name
+}
+
+func typeFrom(name string) reflect.Type {
+	for _, t := range hbookTypes {
+		if name == nameFromType(t) {
+			return t
+		}
+	}
+	return nil
+}
+
+var hbookTypes = []reflect.Type{
+	reflect.TypeOf((*hbook.H1D)(nil)),
+	reflect.TypeOf((*hbook.H2D)(nil)),
+	reflect.TypeOf((*hbook.P1D)(nil)),
+	reflect.TypeOf((*hbook.S2D)(nil)),
+}
+
+type yodaMarshaler interface {
+	MarshalYODA() ([]byte, error)
+}
