504c7da5c3f37ee97f72a25b9b79c2b8db3bc67f
Sebastien Binet <binet@cern.ch>
2017-03-03 09:53:48 +0100

    rio/cmd/rio-ls-records: add doc + license header

--- rio/cmd/rio-ls-records/main.go
+++ rio/cmd/rio-ls-records/main.go
@@ -1,3 +1,8 @@
+// Copyright 2017 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// rio-ls-records displays the list of records stored in a given rio file.
 package main
 
 import (
