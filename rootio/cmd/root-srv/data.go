427cc755a08141d26f60b8cd6d828dc7a11ef004
Sebastien Binet <binet@cern.ch>
2017-03-22 21:14:55 +0100

    rootio/cmd/root-srv: add support for plotting H1D and TFile navigation

---
+++ rootio/cmd/root-srv/data.go
@@ -0,0 +1,209 @@
+// Copyright 2017 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"math"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/gonum/plot/vg"
+	"github.com/gonum/plot/vg/draw"
+	"github.com/gonum/plot/vg/vgsvg"
+
+	"go-hep.org/x/hep/hplot"
+	"go-hep.org/x/hep/rootio"
+)
+
+type Page struct {
+	Token string
+	Path  string
+
+	FileTree template.HTML
+	Display  string
+}
+
+func (p Page) DoFileTree() bool {
+	return p.FileTree != ""
+}
+
+func (p Page) DoDisplay() bool {
+	return p.Display != ""
+}
+
+type dbFiles struct {
+	sync.RWMutex
+	files map[string]*rootio.File
+}
+
+func (db *dbFiles) get(name string) *rootio.File {
+	db.RLock()
+	defer db.RUnlock()
+	f, _ := db.files[name]
+	return f
+}
+
+func (db *dbFiles) set(name string, f *rootio.File) {
+	db.Lock()
+	defer db.Unlock()
+	if old, dup := db.files[name]; dup {
+		old.Close()
+	}
+	db.files[name] = f
+}
+
+func (db *dbFiles) del(name string) {
+	db.Lock()
+	defer db.Unlock()
+	f, ok := db.files[name]
+	if !ok {
+		return
+	}
+	f.Close()
+	delete(db.files, name)
+}
+
+func session(r *http.Request, name string) string {
+	// FIXME(sbinet): use a real cookie+session to mangle file name
+	return name
+}
+
+type jsNode struct {
+	ID    string `json:"id,omitempty"`
+	Text  string `json:"text,omitempty"`
+	Icon  string `json:"icon,omitempty"`
+	State struct {
+		Opened   bool `json:"opened,omitempty"`
+		Disabled bool `json:"disabled,omitempty"`
+		Selected bool `json:"selected,omitempty"`
+	} `json:"state,omitempty"`
+	Children []jsNode `json:"children,omitempty"`
+	LiAttr   jsAttr   `json:"li_attr,omitempty"`
+	Attr     jsAttr   `json:"a_attr,omitempty"`
+}
+
+type jsAttr map[string]interface{}
+
+type brancher interface {
+	Branches() []rootio.Branch
+}
+
+func newJsNodes(bres brancher, id string) ([]jsNode, error) {
+	var err error
+	branches := bres.Branches()
+	if len(branches) <= 0 {
+		return nil, err
+	}
+	var nodes []jsNode
+	for _, b := range branches {
+		node := jsNode{
+			ID:   strings.Join([]string{id, b.Name()}, "/"),
+			Text: b.Name(),
+			Icon: "fa fa-leaf",
+		}
+		node.LiAttr = jsAttr{
+			"href": "/plot/" + node.ID,
+		}
+		node.Attr = jsAttr{
+			"href": "/plot2d/" + node.ID,
+			"plot": true,
+		}
+		node.Children, err = newJsNodes(b, node.ID)
+		if err != nil {
+			return nil, err
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes, nil
+}
+
+func fileJsTree(f *rootio.File, fname string) ([]jsNode, error) {
+	root := jsNode{
+		Text: fmt.Sprintf("%s (version=%v)", fname, f.Version()),
+		Icon: "fa fa-file",
+	}
+	root.State.Opened = true
+	var nodes []jsNode
+	for _, k := range f.Keys() {
+		obj, err := k.Object()
+		if err != nil {
+			return nil, fmt.Errorf("failed to extract key %q: %v", k.Name(), err)
+		}
+		switch obj := obj.(type) {
+		case rootio.Tree:
+			tree := obj
+			node := jsNode{
+				ID:   strings.Join([]string{fname, k.Name()}, "/"),
+				Text: fmt.Sprintf("%s (entries=%d)", k.Name(), tree.Entries()),
+				Icon: "fa fa-tree",
+			}
+			node.Children, err = newJsNodes(tree, node.ID)
+			if err != nil {
+				return nil, err
+			}
+			nodes = append(nodes, node)
+		default:
+			id := strings.Join([]string{fname, k.Name() + fmt.Sprintf(";%d", k.Cycle())}, "/")
+			nodes = append(nodes, jsNode{
+				ID:   id,
+				Text: fmt.Sprintf("%s;%d", k.Name(), k.Cycle()),
+				Icon: iconFor(obj),
+				Attr: attrFor(obj, id),
+			})
+
+		}
+	}
+	root.Children = nodes
+	return []jsNode{root}, nil
+}
+
+func iconFor(obj rootio.Object) string {
+	cls := obj.Class()
+	switch {
+	case strings.HasPrefix(cls, "TH1"):
+		return "fa fa-bar-chart-o"
+	case strings.HasPrefix(cls, "TH2"):
+		return "fa fa-bar-chart-o"
+	}
+	return "fa fa-cube"
+}
+
+func attrFor(obj rootio.Object, id string) jsAttr {
+	cls := obj.Class()
+	switch {
+	case strings.HasPrefix(cls, "TH1"):
+		return jsAttr{
+			"plot": true,
+			"href": "/plot/" + id,
+		}
+	case strings.HasPrefix(cls, "TH2"):
+		return jsAttr{
+			"plot": true,
+			"href": "/plot/" + id,
+		}
+	case strings.HasPrefix(cls, "TBranch"):
+		return jsAttr{
+			"plot": true,
+			"href": "/plot-branch/" + id,
+		}
+	}
+	return nil
+}
+
+func renderSVG(p *hplot.Plot) ([]byte, error) {
+	size := 20 * vg.Centimeter
+	canvas := vgsvg.New(size, size/vg.Length(math.Phi))
+	p.Draw(draw.New(canvas))
+	out := new(bytes.Buffer)
+	_, err := canvas.WriteTo(out)
+	if err != nil {
+		return nil, err
+	}
+	return out.Bytes(), nil
+}
--- rootio/cmd/root-srv/main.go
+++ rootio/cmd/root-srv/main.go
@@ -5,30 +5,41 @@
 package main
 
 import (
-	"bytes"
 	"crypto/md5"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
+	"image/color"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
-	"text/tabwriter"
 	"time"
 
+	"go-hep.org/x/hep/hbook/rootcnv"
+	"go-hep.org/x/hep/hbook/yodacnv"
+	"go-hep.org/x/hep/hplot"
 	"go-hep.org/x/hep/rootio"
 )
 
 var (
 	addrFlag = flag.String("addr", ":8080", "server address:port")
+
+	db = dbFiles{
+		files: make(map[string]*rootio.File),
+	}
 )
 
 func main() {
 	flag.Parse()
 
 	http.Handle("/", appHandler(rootHandle))
+	http.Handle("/root-file-upload", appHandler(uploadHandle))
+	http.Handle("/plot/", appHandler(plotH1Handle))
+	http.Handle("/plot2d/", appHandler(plotH2Handle))
+	log.Printf("server listening on %s", *addrFlag)
 	log.Fatal(http.ListenAndServe(*addrFlag, nil))
 }
 
@@ -36,154 +47,219 @@ type appHandler func(http.ResponseWriter, *http.Request) error
 
 func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err := fn(w, r); err != nil {
+		log.Printf("error %q: %v\n", r.URL.Path, err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func rootHandle(w http.ResponseWriter, r *http.Request) error {
 	switch r.Method {
-	case "GET":
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
-
-		t, err := template.New("upload").Parse(page)
-		if err != nil {
-			return err
-		}
-
-		var data = struct {
-			Token string
-			Path  string
-		}{
-			Token: token,
-			Path:  strings.Replace(r.URL.Path+"/root-file-upload", "//", "/", -1),
-		}
-
-		err = t.Execute(w, data)
-		if err != nil {
-			return err
-		}
-
-	case "POST":
-		r.ParseMultipartForm(500 << 20)
-		f, handler, err := r.FormFile("upload-file")
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		_, err = f.Seek(0, 0)
-		if err != nil {
-			return err
-		}
-
-		out, err := inspect(f, handler.Filename)
-		if err != nil {
-			return err
-		}
-
-		fmt.Fprintf(w, out)
-
+	case http.MethodGet:
+		// ok
 	default:
-		return fmt.Errorf("invalid request %q", r.Method)
+		return fmt.Errorf("invalid request %q for /", r.Method)
 	}
 
-	return nil
+	crutime := time.Now().Unix()
+	h := md5.New()
+	io.WriteString(h, strconv.FormatInt(crutime, 10))
+	token := fmt.Sprintf("%x", h.Sum(nil))
+
+	t, err := template.New("upload").Parse(page)
+	if err != nil {
+		return err
+	}
+
+	var data = Page{
+		Token: token,
+		Path:  strings.Replace(r.URL.Path+"/root-file-upload", "//", "/", -1),
+	}
+
+	return t.Execute(w, data)
+}
+
+func uploadHandle(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != http.MethodPost {
+		log.Printf("invalid request %q", r.Method)
+		return fmt.Errorf("invalid request %q for /root-file-upload", r.Method)
+	}
+
+	r.ParseMultipartForm(500 << 20)
+	f, handler, err := r.FormFile("upload-file")
+	if err != nil {
+		return err
+	}
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+
+	rfile, err := rootio.NewReader(f, handler.Filename)
+	if err != nil {
+		return err
+	}
+	db.set(session(r, handler.Filename), rfile)
+
+	nodes, err := fileJsTree(rfile, handler.Filename)
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(w).Encode(nodes)
+}
+
+func plotH2Handle(w http.ResponseWriter, r *http.Request) error {
+	log.Printf(">>> request: %q\n", r.URL.Path)
+
+	return json.NewEncoder(w).Encode(map[string]string{
+		"url": r.URL.Path,
+	})
+}
+
+func plotH1Handle(w http.ResponseWriter, r *http.Request) error {
+	log.Printf(">>> request: %q\n", r.URL.Path)
+	url := r.URL.Path[len("/plot/"):]
+	toks := strings.Split(url, "/")
+	fname := toks[0]
+	f := db.get(session(r, fname))
+	obj, ok := f.Get(toks[1]) // FIXME(sbinet): handle sub-dirs
+	if !ok {
+		return fmt.Errorf("could not find %q in file %q", toks[1], fname)
+	}
+
+	robj, ok := obj.(yodacnv.Marshaler)
+	if !ok {
+		return fmt.Errorf("object %q could not be converted to hbook.H1D", toks[1])
+	}
+	h1d, err := rootcnv.H1D(robj)
+	if err != nil {
+		return err
+	}
+
+	plot, err := hplot.New()
+	if err != nil {
+		return err
+	}
+	plot.Title.Text = obj.(rootio.Named).Title()
+
+	h, err := hplot.NewH1D(h1d)
+	if err != nil {
+		return err
+	}
+	h.Infos.Style = hplot.HInfoSummary
+	h.Color = color.RGBA{255, 0, 0, 255}
+
+	plot.Add(h, hplot.NewGrid())
+
+	svg, err := renderSVG(plot)
+	if err != nil {
+		return err
+	}
+
+	return json.NewEncoder(w).Encode(string(svg))
 }
 
 const page = `<html>
 <head>
     <title>go-hep/rootio file inspector</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/themes/default/style.min.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/jstree.min.js"></script>
+	<style>
+	input[type=file] {
+		display: none;
+	}
+	input[type=submit] {
+		background-color: #F44336;
+		padding:5px 15px;
+		border:0 none;
+		cursor:pointer;
+		-webkit-border-radius: 5px;
+		border-radius: 5px;
+	}
+	.flex-container {
+		display: -webkit-flex;
+		display: flex;
+	}
+	.flex-item {
+		margin: 5px;
+	}
+	.rootio-file-upload {
+		color: white;
+		background-color: #0091EA;
+		padding:5px 15px;
+		border:0 none;
+		cursor:pointer;
+		-webkit-border-radius: 5px;
+	}
+	.rootio-browser {
+		resize: horizontal;
+		overflow: auto;
+		padding:5px 15px;
+		float: left;
+		height: 95%;
+	}
+	</style>
 </head>
 <body>
-<h2>go-hep/rootio ROOT file inspector</h2>
-<form id="rootio-form" enctype="multipart/form-data" action={{.Path}} method="post">
-      <input id="rootio-file" type="file" name="upload-file" />
-      <input type="hidden" name="token" value="{{.Token}}"/>
-      <input type="submit" value="upload" />
-</form>
+<div class="flex-container w3-light-grey">
+	<div class="rootio-browser flex-item w3-container w3-card w3-white">
+		<div>
+			<h2>go-hep/rootio ROOT file inspector</h2>
+		</div>
+		<form id="rootio-form" enctype="multipart/form-data" action="{{.Path}}" method="post">
+			<label for="rootio-file" class="rootio-file-upload" style="font-size:16px">
+			<i class="fa fa-cloud-upload" aria-hidden="true" style="font-size:16px"></i> Upload
+			</label>
+			<input id="rootio-file" type="file" name="upload-file"/>
+			<input type="hidden" name="token" value="{{.Token}}"/>
+			<input type="hidden" value="upload" />
+		</form>
+		<div id="rootio-file-tree" class="rootio-file-tree flex-item">
+		</div>
+	</div>
+	<div class="flex-item w3-container w3-content" id="rootio-display">
+	</div>
+</div>
 <script type="text/javascript">
 	document.getElementById("rootio-file").onchange = function() {
-		document.getElementById("rootio-form").submit();
+		var data = new FormData($("#rootio-form")[0]);
+		$.ajax({
+			url: "{{.Path}}",
+			method: "POST",
+			data: data,
+			processData: false,
+			contentType: false,
+			success: function(result){
+				// console.log("json-result: "+result);
+				$('#rootio-file-tree').jstree(true).settings.core.data = JSON.parse(result);
+				$("#rootio-file-tree").jstree(true).refresh();
+			},
+			error: function(er){
+				alert("upload failed: "+er);
+			}
+		});
 	}
+	$(function () {
+		$('#rootio-file-tree').jstree();
+		$("#rootio-file-tree").on("select_node.jstree",
+			function(evt, data){
+				data.instance.toggle_node(data.node);
+				if (data.node.a_attr.plot) {
+					$.get(data.node.a_attr.href, plotCallback);
+				}
+			}
+		);
+	});
+
+	function plotCallback(data, status) {
+		var node = $("<div></div>");
+		node.addClass("w3-panel w3-white w3-card-2 w3-display-middle");
+		node.html(JSON.parse(data));
+		$("#rootio-display").html(node);
+	};
 </script>
 </body>
 </html>
 `
-
-func inspect(r rootio.Reader, fname string) (string, error) {
-	f, err := rootio.NewReader(r, fname)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	w := new(bytes.Buffer)
-	fmt.Fprintf(w, "=== inspecting file %q...\n", fname)
-	fmt.Fprintf(w, "version: %v\n", f.Version())
-	ww := tabwriter.NewWriter(w, 8, 4, 1, ' ', 0)
-	for _, k := range f.Keys() {
-		obj, err := k.Object()
-		if err != nil {
-			return "", fmt.Errorf("failed to extract key %q: %v", k.Name(), err)
-		}
-		switch obj := obj.(type) {
-		case rootio.Tree:
-			tree := obj
-			ww := tabwriter.NewWriter(ww, 8, 4, 1, ' ', 0)
-			fmt.Fprintf(ww, "%s\t%s\t%s\t(entries=%d)\n", k.Class(), k.Name(), k.Title(), tree.Entries())
-			displayBranches(ww, tree, 2)
-			ww.Flush()
-		default:
-			fmt.Fprintf(ww, "%s\t%s\t%s\t(cycle=%d)\n", k.Class(), k.Name(), k.Title(), k.Cycle())
-		}
-	}
-	ww.Flush()
-	return string(w.Bytes()), nil
-}
-
-type windent struct {
-	hdr []byte
-	w   io.Writer
-}
-
-func newWindent(n int, w io.Writer) *windent {
-	return &windent{
-		hdr: bytes.Repeat([]byte(" "), n),
-		w:   w,
-	}
-}
-
-func (w *windent) Write(data []byte) (int, error) {
-	return w.w.Write(append(w.hdr, data...))
-}
-
-func (w *windent) Flush() error {
-	ww, ok := w.w.(flusher)
-	if !ok {
-		return nil
-	}
-	return ww.Flush()
-}
-
-type flusher interface {
-	Flush() error
-}
-
-type brancher interface {
-	Branches() []rootio.Branch
-}
-
-func displayBranches(w io.Writer, bres brancher, indent int) {
-	branches := bres.Branches()
-	if len(branches) <= 0 {
-		return
-	}
-	ww := newWindent(indent, w)
-	for _, b := range branches {
-		fmt.Fprintf(ww, "%s\t%q\t%v\n", b.Name(), b.Title(), b.Class())
-		displayBranches(ww, b, 2)
-	}
-	ww.Flush()
-}
