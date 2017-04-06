24580d89712c776adb0f269db1fb31a15a367938
Sebastien Binet <binet@cern.ch>
2017-04-06 15:18:15 +0200

    rootio/cmd/root-srv: improve documentation

--- rootio/cmd/root-srv/main.go
+++ rootio/cmd/root-srv/main.go
@@ -2,12 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// root-srv runs a web server that can inspect and browse ROOT files.
+// root-srv can also display ROOT objects (TH1x, TH2x, TGraphs, TGraphErrors,
+// TGraphAsymmErrors, TDirectories, TTrees, ...).
+//
+// Usage: root-srv [options]
+//
+// ex:
+//
+//  $> root-srv -addr=:8080 &
+//  2017/04/06 15:13:59 server listening on :8080
+//
+//  $> open localhost:8080
+//
 package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"go-hep.org/x/hep/rootio/cmd/root-srv/server"
 )
@@ -17,6 +32,22 @@ var (
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(
+			os.Stderr,
+			`Usage: root-srv [options]
+
+ex:
+
+ $> root-srv -addr=:8080
+ 2017/04/06 15:13:59 server listening on :8080
+
+options:
+`,
+		)
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
 	server.Init()
