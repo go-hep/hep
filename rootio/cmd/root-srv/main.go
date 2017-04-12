5b287003e2548502b60637463a1494d354866bc5
Michaël Ughetto <mughetto@lap-mughe.fysik.su.se>
2017-04-12 10:29:05 +0200

    Work in progress for tests on localhost..

--- rootio/cmd/root-srv/main.go
+++ rootio/cmd/root-srv/main.go
@@ -22,9 +22,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+//	"crypto/tls"
 	"os"
 
+	"golang.org/x/crypto/acme/autocert"
+	
 	"go-hep.org/x/hep/rootio/cmd/root-srv/server"
+	
 )
 
 var (
@@ -49,8 +53,35 @@ options:
 	}
 
 	flag.Parse()
-
 	server.Init()
-	log.Printf("server listening on %s", *addrFlag)
-	log.Fatal(http.ListenAndServe(*addrFlag, nil))
+
+//	certManager := autocert.Manager{
+//		Prompt:     autocert.AcceptTOS,
+//		HostPolicy: autocert.HostWhitelist("example.com"), //your domain here
+//		Cache:      autocert.DirCache("certs"), //folder for storing certificates
+//	}
+//	
+//
+//	// piqué sur sx
+//	server := &http.Server{
+//		Addr: ":443",
+//		TLSConfig: &tls.Config{
+//			GetCertificate: certManager.GetCertificate,
+//		},
+//	}
+//
+//	server.ListenAndServeTLS("", "")
+	
+	log.Printf("Http server listening on %s", *addrFlag)
+//	log.Fatal(http.ListenAndServe(*addrFlag, nil))
+	//	log.Fatal(http.Serve(autocert.NewListener("localhost"),nil ))
+
+	m := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("127.0.0.1"),
+		Cache:      autocert.DirCache("certs"), //folder for storing certificates
+	}
+
+	log.Fatal(http.Serve(m.Listener(),nil ))
+	
 }
