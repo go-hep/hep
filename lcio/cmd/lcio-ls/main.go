e5ff5acdc4fa3bee2ca070e444f1b4be77fd72f5
Sebastien Binet <binet@cern.ch>
2017-04-20 18:23:45 +0200

    lcio/cmd/lcio-ls: first import

---
+++ lcio/cmd/lcio-ls/main.go
@@ -0,0 +1,72 @@
+// Copyright 2017 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"text/tabwriter"
+
+	"go-hep.org/x/hep/lcio"
+)
+
+func main() {
+	log.SetPrefix("lcio-ls: ")
+	log.SetFlags(0)
+
+	var (
+		fname = ""
+		nevts = flag.Int64("n", -1, "number of events to inspect")
+	)
+
+	flag.Parse()
+
+	if flag.NArg() > 0 {
+		fname = flag.Arg(0)
+	}
+
+	if fname == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	log.Printf("inspecting file [%s]...", fname)
+	r, err := lcio.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	rhdr := r.RunHeader()
+	for ievt := int64(0); r.Next() && (*nevts < 0 || ievt < *nevts); ievt++ {
+		evt := r.Event()
+		if hdr := r.RunHeader(); !reflect.DeepEqual(hdr, rhdr) {
+			log.Printf("=== run-header ===\n%v", hdr)
+			rhdr = hdr
+		}
+		log.Printf("ievt[%d]", ievt)
+		out := new(bytes.Buffer)
+		w := tabwriter.NewWriter(out, 8, 4, 1, ' ', 0)
+
+		for _, n := range evt.Names() {
+			v := evt.Get(n)
+			fmt.Fprintf(w, " %s\t%T\n", n, v)
+		}
+		w.Flush()
+		sc := bufio.NewScanner(out)
+		for sc.Scan() {
+			log.Printf("%s\n", sc.Text())
+		}
+	}
+	err = r.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
