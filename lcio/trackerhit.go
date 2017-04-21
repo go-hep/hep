3025c0022655dd19508dfbb0ed8234e39dde20cc
Sebastien Binet <binet@cern.ch>
2017-04-21 18:09:15 +0200

    lcio: add trackerhit stringer

--- lcio/trackerhit.go
+++ lcio/trackerhit.go
@@ -5,6 +5,10 @@
 package lcio
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
+
 	"go-hep.org/x/hep/sio"
 )
 
@@ -15,12 +19,90 @@ type TrackerHitContainer struct {
 	Hits   []TrackerHit
 }
 
+type TrackerHit struct {
+	CellID0 int32
+	CellID1 int32
+	Type    int32 // type of Track; encoded in parameters TrackerHitTypeName+TrackerHit TypeValue
+	Pos     [3]float64
+	Cov     [6]float64 // covariance matrix of position (x,y,z)
+	EDep    float32    // energy deposit on the hit
+	EDepErr float32    // error measured on EDep
+	Time    float32
+	Quality int32 // quality flag word
+	RawHits []*RawCalorimeterHit
+}
+
+func (hits TrackerHitContainer) String() string {
+	o := new(bytes.Buffer)
+	fmt.Fprintf(o, "%[1]s print out of TrackerHit collection %[1]s\n\n", strings.Repeat("-", 15))
+	fmt.Fprintf(o, "  flag:  0x%x\n%v", hits.Flags, hits.Params)
+	fmt.Fprintf(o, "     LCIO::THBIT_BARREL   : %v\n", hits.Flags.Test(BitsThBarrel))
+
+	// FIXME(sbinet): quality-bits
+
+	// FIXME(sbinet): CellIDDecoder
+
+	fmt.Fprintf(o, "\n")
+
+	const (
+		head = " [   id   ] |cellId0 |cellId1 | position (x,y,z)            | time    |[type]|[qual]| EDep    |EDepError|  cov(x,x),  cov(y,x),  cov(y,y),  cov(z,x),  cov(z,y),  cov(z,z)\n"
+		tail = "------------|--------|--------|-----------------------------|---------|------|------|---------|---------|-----------------------------------------------------------------\n"
+	)
+	fmt.Fprintf(o, head)
+	fmt.Fprintf(o, tail)
+	for i := range hits.Hits {
+		hit := &hits.Hits[i]
+		fmt.Fprintf(o,
+			" [%08d] |%08d|%08d|%+.2e,%+.2e,%+.2e|%+.2e|[%04d]|[%04d]|%+.2e|%+.2e|%+.2e, %+.2e, %+.2e, %+.2e, %+.2e, %+.2e\n",
+			0, // id
+			hit.CellID0, hit.CellID1,
+			hit.Pos[0], hit.Pos[1], hit.Pos[2],
+			hit.Time, hit.Type, hit.Quality,
+			hit.EDep, hit.EDepErr,
+			hit.Cov[0], hit.Cov[1], hit.Cov[2], hit.Cov[3], hit.Cov[4], hit.Cov[5],
+		)
+	}
+	fmt.Fprintf(o, tail)
+	return string(o.Bytes())
+}
+
 func (*TrackerHitContainer) VersionSio() uint32 {
 	return Version
 }
 
 func (hits *TrackerHitContainer) MarshalSio(w sio.Writer) error {
-	panic("not implemented")
+	enc := sio.NewEncoder(w)
+	enc.Encode(&hits.Flags)
+	enc.Encode(&hits.Params)
+	enc.Encode(int32(len(hits.Hits)))
+	for i := range hits.Hits {
+		hit := &hits.Hits[i]
+		enc.Encode(&hit.CellID0)
+		if hits.Flags.Test(BitsThID1) {
+			enc.Encode(&hit.CellID1)
+		}
+		enc.Encode(&hit.Type)
+		enc.Encode(&hit.Pos)
+		var cov [6]float32
+		cov[0] = float32(hit.Cov[0])
+		cov[1] = float32(hit.Cov[1])
+		cov[2] = float32(hit.Cov[2])
+		cov[3] = float32(hit.Cov[3])
+		cov[4] = float32(hit.Cov[4])
+		cov[5] = float32(hit.Cov[5])
+		enc.Encode(&cov)
+		enc.Encode(&hit.EDep)
+		enc.Encode(&hit.EDepErr)
+		enc.Encode(&hit.Time)
+		enc.Encode(&hit.Quality)
+
+		enc.Encode(int32(len(hit.RawHits)))
+		for ii := range hit.RawHits {
+			enc.Pointer(&hit.RawHits[ii])
+		}
+		enc.Tag(hit)
+	}
+	return enc.Err()
 }
 
 func (hits *TrackerHitContainer) UnmarshalSio(r sio.Reader) error {
@@ -72,19 +154,6 @@ func (hits *TrackerHitContainer) UnmarshalSio(r sio.Reader) error {
 	}
 
 	return dec.Err()
-}
-
-type TrackerHit struct {
-	CellID0 int32
-	CellID1 int32
-	Type    int32 // type of Track; encoded in parameters TrackerHitTypeName+TrackerHit TypeValue
-	Pos     [3]float64
-	Cov     [6]float64 // covariance matrix of position (x,y,z)
-	EDep    float32    // energy deposit on the hit
-	EDepErr float32    // error measured on EDep
-	Time    float32
-	Quality int32 // quality flag word
-	RawHits []*RawCalorimeterHit
 }
 
 var (
