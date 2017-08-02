291b8f35d7ee4e2c042ed1639f1bee2af84de34d
Bastian Wieck <b.wieck@gmx.de>
2017-08-02 10:31:18 -0400

    predicates: add tests

--- fastjet/internal/predicates/orientation_test.go
+++ fastjet/internal/predicates/orientation_test.go
@@ -58,3 +58,75 @@ func TestSimpleVsRobustOrientation(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkSimpleOrientation(b *testing.B) {
+	tests := []struct {
+		x1, y1, x2, y2, x, y float64
+	}{
+		{1, 1, 1, 5, 1, 3},
+		{2, 2, 10, 2, 7, 2},
+		{2, 2, 4, 4, 3, 3},
+		{1, 1, 1, 5, 0, 3},
+		{1, 1, 1, 5, 2, 3},
+		{2, 2, 10, 2, 7, 2.001},
+		{2, 2, 10, 2, 7, 1.999},
+		{2, 2, 5, 3, 3, 2.334},
+		{2, 2, 5, 3, 3, 2.333},
+		{0, 11, 4, 10, 7, 9.251},
+		{0, 11, 4, 10, 7, 9.249},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, test := range tests {
+			simpleOrientation(test.x1, test.y1, test.x2, test.y2, test.x, test.y)
+		}
+	}
+}
+
+func BenchmarkMatOrientation(b *testing.B) {
+	tests := []struct {
+		x1, y1, x2, y2, x, y float64
+	}{
+		{1, 1, 1, 5, 1, 3},
+		{2, 2, 10, 2, 7, 2},
+		{2, 2, 4, 4, 3, 3},
+		{1, 1, 1, 5, 0, 3},
+		{1, 1, 1, 5, 2, 3},
+		{2, 2, 10, 2, 7, 2.001},
+		{2, 2, 10, 2, 7, 1.999},
+		{2, 2, 5, 3, 3, 2.334},
+		{2, 2, 5, 3, 3, 2.333},
+		{0, 11, 4, 10, 7, 9.251},
+		{0, 11, 4, 10, 7, 9.249},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, test := range tests {
+			matOrientation(test.x1, test.y1, test.x2, test.y2, test.x, test.y)
+		}
+	}
+}
+
+func BenchmarkRobustOrientation(b *testing.B) {
+	tests := []struct {
+		x1, y1, x2, y2, x, y float64
+	}{
+		{1, 1, 1, 5, 1, 3},
+		{2, 2, 10, 2, 7, 2},
+		{2, 2, 4, 4, 3, 3},
+		{1, 1, 1, 5, 0, 3},
+		{1, 1, 1, 5, 2, 3},
+		{2, 2, 10, 2, 7, 2.001},
+		{2, 2, 10, 2, 7, 1.999},
+		{2, 2, 5, 3, 3, 2.334},
+		{2, 2, 5, 3, 3, 2.333},
+		{0, 11, 4, 10, 7, 9.251},
+		{0, 11, 4, 10, 7, 9.249},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, test := range tests {
+			robustOrientation(setBig(test.x1), setBig(test.y1), setBig(test.x2), setBig(test.y2), setBig(test.x), setBig(test.y))
+		}
+	}
+}
