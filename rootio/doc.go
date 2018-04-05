f565d9c0fce5540c7c154ae857690f5e7c93a8d5
Sebastien Binet <binet@cern.ch>
2018-04-05 14:31:31 +0200

    rootio: add documentation of ROOT file binary layout

--- rootio/doc.go
+++ rootio/doc.go
@@ -27,4 +27,93 @@
 // Another possibility is to look at:
 // https://godoc.org/go-hep.org/x/hep/rootio/cmd/root-ls,
 // a command that inspects the content of ROOT files.
+//
+//
+// File layout
+//
+// ROOT files are a suite of consecutive data records.
+// Each data record consists of a header part, called a TKey, and a payload
+// whose content, length and meaning are described by the header.
+// The current ROOT file format encodes all data in big endian.
+//
+// ROOT files initially only supported 32b addressing.
+// Large files support (>4Gb) was added later on by migrating to a 64b addressing.
+//
+// The on-disk binary layout of a ROOT file header looks like this:
+//
+//       Type        | Record Name | Description
+//  =================+=============+===========================================
+//     [4]byte       | "root"      | Root file identifier
+//     int32         | fVersion    | File format version
+//     int32         | fBEGIN      | Pointer to first data record
+//     int32 [int64] | fEND        | Pointer to first free word at the EOF
+//     int32 [int64] | fSeekFree   | Pointer to FREE data record
+//     int32         | fNbytesFree | Number of bytes in FREE data record
+//     int32         | nfree       | Number of free data records
+//     int32         | fNbytesName | Number of bytes in TNamed at creation time
+//     byte          | fUnits      | Number of bytes for file pointers
+//     int32         | fCompress   | Compression level and algorithm
+//     int32 [int64] | fSeekInfo   | Pointer to TStreamerInfo record
+//     int32         | fNbytesInfo | Number of bytes in TStreamerInfo record
+//     [18]byte      | fUUID       | Universal Unique ID
+//  =================+=============+===========================================
+//
+// This is followed by a sequence of data records, starting at the fBEGIN
+// offset from the beginning of the file.
+//
+// The on-disk binary layout of a data record is:
+//
+//        Type     | Member Name | Description
+//  ===============+=============+===========================================
+//   int32         | Nbytes      | Length of compressed object (in bytes)
+//   int16         | Version     | TKey version identifier
+//   int32         | ObjLen      | Length of uncompressed object
+//   int32         | Datime      | Date and time when object was written to file
+//   int16         | KeyLen      | Length of the key structure (in bytes)
+//   int16         | Cycle       | Cycle of key
+//   int32 [int64] | SeekKey     | Pointer to record itself (consistency check)
+//   int32 [int64] | SeekPdir    | Pointer to directory header
+//   byte          | lname       | Number of bytes in the class name
+//   []byte        | ClassName   | Object Class Name
+//   byte          | lname       | Number of bytes in the object name
+//   []byte        | Name        | Name of the object
+//   byte          | lTitle      | Number of bytes in the object title
+//   []byte        | Title       | Title of the object
+//   []byte        | DATA        | Data bytes associated to the object
+//  ===============+=============+===========================================
+//
+// The high-level on-disk representation of a ROOT file is thus:
+//
+//  +===============+ -- 0
+//  |               |
+//  |  File Header  |
+//  |               |
+//  +===============+ -- fBEGIN offset
+//  |               |
+//  | Record Header | ----+
+//  |               |     |
+//  +===============+     |
+//  |               |     |
+//  |  Record Data  |     | Reference to next Record
+//  |    Payload    |     |
+//  |               |     |
+//  +===============+ <---+
+//  |               |
+//  | Record Header | ----+
+//  |               |     |
+//  +===============+     |
+//  |               |     |
+//  |  Record Data  |     | Reference to next Record
+//  |    Payload    |     |
+//  |               |     |
+//  +===============+ <---+
+//  |               |
+//         ...
+//
+//  |               |
+//  +===============+ -- fEND offset
+//
+// Data records payloads and how to deserialize them are described by a TStreamerInfo.
+// The list of all TStreamerInfos that are used to interpret the content of
+// a ROOT file is stored at the end of that ROOT file, at offset fSeekInfo.
 package rootio // import "go-hep.org/x/hep/rootio"
