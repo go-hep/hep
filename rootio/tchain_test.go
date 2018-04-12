0c651acce66563e2f5078bde28435a613446164d
Mohamed Amine EL GNAOUI <mohamed.amine.elgnaoui@gmail.com>
2018-04-12 17:06:44 +0200

    rootio: first stab at Chain test

---
+++ rootio/tchain_test.go
@@ -0,0 +1,34 @@
+// Copyright 2018 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rootio_test
+
+import (
+	"testing"
+
+	"go-hep.org/x/hep/rootio"
+)
+
+func TestChain(t *testing.T) {
+	f, err := rootio.Open("testdata/chain.1.root")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	obj, err := f.Get("tree")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tree := obj.(rootio.Tree)
+	chain := rootio.Chain(tree)
+
+	if etree, echain := tree.Entries(), chain.Entries(); etree != echain {
+		t.Fatalf("entries differ. got=%v, want=%v", echain, etree)
+	}
+}
+
+func TestChainEmpty(t *testing.T) {
+chain := rootio.Chain()
+}
