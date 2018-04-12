a692e8b52a9ca575a275cebd4e41e065ce591da3
Mohamed Amine EL GNAOUI <mohamed.amine.elgnaoui@gmail.com>
2018-04-12 13:27:56 +0200

    rootio: update

--- rootio/tchain.go
+++ rootio/tchain.go
@@ -1,4 +1,7 @@
-//An update of go-hep/hep#44
+// Copyright 2018 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 
 
 package rootio
@@ -13,94 +16,7 @@ func (tchain) Class() string {
 	return "TChain"
 }
 
-// Chain returns a tchain that is the concatenation of all the input Trees.
-func Chain(trees ...Tree) tchain {
-	var t tchain
-	t.trees = append(t.trees, trees...)
-	return t
-}
 
-
-//Entries returns the total number of entries 
-func (t tchain) Entries() int64 {
-	var v int64 = 0
-	for i := range t.trees {
-		v = v + t.trees[i].Entries()
-	}
-	return v
-
-}
-	
-
-// TotBytes return the total number of bytes before compression.
-func (t tchain) TotBytes() int64 {
-	var v int64 = 0
-	for i := range t.trees {
-		v = v + t.trees[i].TotBytes()
-	}
-	return v
-
-}
-
-//ZipBytes returns the total number of bytes after compression.
-func (t tchain) ZipBytes() int64 {
-	var v int64 = 0
-	for i := range t.trees {
-		v = v + t.trees[i].ZipBytes()
-	}
-	return v
-
-}
-
-
-//Branches returns the list of branches.
-func (t tchain) Branches() []Branch {
-
-	return t.trees[0].Branches()
-	
-}
-
-
-//Branch returns the branch whose name is the argument.
-func (t tchain) Branch(name string) Branch {
-
-	for _, br := range t.trees[0].Branches() {
-		if br.Name() == name {
-			return br
-		}
-	}
-	return nil
-}
-
-var (
-	_ Tree = (*tchain)(nil)
-)
-
-
-//Leaves returns direct pointers to individual branch leaves.
-func (t tchain) Leaves() []Leaf {
-
-	return t.trees[0].Leaves()
-}
-
-
-//getFile returns the underlying file.
-func (t tchain) getFile() *File {
-
-	return t.trees[0].getFile()
-}
-
-
-//loadEntry returns an error if there is a problem during the loading
-func (t tchain) loadEntry(i int64) error {
-	for _, b := range t.trees[0].Branches() {
-		err := b.loadEntry(i)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
 
 //Name returns the name of the ROOT objet in the argument.
 func (t tchain) Name() string {
@@ -112,3 +28,102 @@ func (t tchain) Name() string {
 func (t tchain) Title() string {
 	return t.trees[0].Title()
 }
+
+// Chain returns a tchain that is the concatenation of all the input Trees.
+func Chain(trees ...Tree) tchain {
+	var t tchain
+	t.trees = append(t.trees, trees...)
+	return t
+}
+
+//Entries returns the total number of entries 
+func (t tchain) Entries() int64 {
+	var v int64 
+	for _, tree := range t.trees {
+		v += tree.Entries()
+	}
+	return v
+}
+	
+// TotBytes return the total number of bytes before compression.
+func (t tchain) TotBytes() int64 {
+	var v int64 
+	for _,tree:= range t.trees {
+		v += tree.TotBytes()
+	}
+	return v
+}
+
+//ZipBytes returns the total number of bytes after compression.
+func (t tchain) ZipBytes() int64 {
+	var v int64 
+	for _,tree:= range t.trees {
+		v += tree.ZipBytes()
+	}
+	return v
+
+}
+
+//Branches returns the list of branches.
+func (t tchain) Branches() []Branch {
+	branch := t.trees[0].Branches()
+	if branch != nil {
+		return branch
+	}
+	return nil	
+}
+
+//Branch returns the branch whose name is the argument.
+func (t tchain) Branch(name string) Branch {
+	branch := t.trees[0].Branches()
+	if branch != nil {	
+		for _, br := range t.trees[0].Branches() {
+			if br.Name() == name {
+				return br
+			}
+		}
+		return nil
+	}
+	return nil
+}
+
+//Leaves returns direct pointers to individual branch leaves.
+func (t tchain) Leaves() []Leaf {
+	leaf := t.trees[0].Leaves()
+	if leaf != nil {
+		return leaf
+	}
+	return nil
+}
+
+//getFile returns the underlying file.
+func (t tchain) getFile() *File {
+	f := t.trees[0].getFile()
+	if f != nil {
+		return f
+	}
+	return nil
+}
+
+//loadEntry returns an error if there is a problem during the loading
+func (t tchain) loadEntry(i int64) error {
+	branch := t.trees[0].Branches()
+	if branch != nil {
+		for _, b := range t.trees[0].Branches() {
+			err := b.loadEntry(i)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	return nil
+}
+
+var (
+	_ Object = (*tchain)(nil)
+	_ Named = (*tchain)(nil)
+	_ Tree = (*tchain)(nil)
+)
+
+
