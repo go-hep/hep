b3a2dbd75e0e7f0c9658b2498ae0a4dd86a3d0d7
Sebastien Binet <binet@cern.ch>
2018-06-04 11:47:37 +0200

    xrootd/client: enable tests against C++ server

--- xrootd/client/cxx_server_test.go
+++ xrootd/client/cxx_server_test.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build xrootd_test_with_cxx_server
-
 package client // import "go-hep.org/x/hep/xrootd/client"
 
 func init() {
