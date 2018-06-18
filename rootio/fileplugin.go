e28f096ee85cab73e4b2e565845eaa391fccd0b8
Sebastien Binet <binet@cern.ch>
2018-06-18 12:26:03 +0200

    rootio: use xrdio to open and read xrootd-served files

--- rootio/fileplugin.go
+++ rootio/fileplugin.go
@@ -5,17 +5,13 @@
 package rootio
 
 import (
-	"context"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 	"strings"
 
-	"github.com/pkg/errors"
-	xrdclient "go-hep.org/x/hep/xrootd/client"
-	"go-hep.org/x/hep/xrootd/xrdfs"
+	"go-hep.org/x/hep/xrootd/xrdio"
 )
 
 func openFile(path string) (Reader, error) {
@@ -47,8 +43,7 @@ func openFile(path string) (Reader, error) {
 		return os.Open(path)
 
 	case strings.HasPrefix(path, "xroot://"), strings.HasPrefix(path, "root://"):
-		f, err := xrdOpen(path)
-		return f, err
+		return xrdio.Open(path)
 
 	default:
 		return os.Open(path)
@@ -65,117 +60,10 @@ func (f *tmpFile) Close() error {
 	return f.File.Close()
 }
 
-func xrdOpen(name string) (*xrdFile, error) {
-	urn, err := url.Parse(name)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	host := urn.Hostname()
-	port := urn.Port()
-
-	path := urn.Path
-	if strings.HasPrefix(path, "//") {
-		path = path[1:]
-	}
-
-	user := ""
-	if urn.User != nil {
-		user = urn.User.Username()
-	}
-
-	addr := host
-	if port != "" {
-		addr += ":" + port
-	}
-
-	xrd, err := xrdclient.NewClient(context.Background(), addr, user)
-	if err != nil {
-		return nil, errors.Errorf("rootio: could not connect to xrootd server %q: %v", host, err)
-	}
-
-	fs := xrd.FS()
-	f, err := fs.Open(context.Background(), path, xrdfs.OpenModeOwnerRead, xrdfs.OpenOptionsOpenRead)
-	if err != nil {
-		xrd.Close()
-		return nil, errors.Errorf("rootio: could not open %q: %v", name, err)
-	}
-
-	return &xrdFile{cli: xrd, fs: fs, f: f, name: name}, nil
-}
-
-type xrdFile struct {
-	cli *xrdclient.Client
-	fs  xrdfs.FileSystem
-	f   xrdfs.File
-
-	name string
-	pos  int64
-}
-
-// Name returns the name of the file.
-func (f *xrdFile) Name() string {
-	return f.name
-}
-
-func (f *xrdFile) Close() error {
-	err1 := f.f.Close(context.Background())
-	err2 := f.cli.Close()
-	if err1 != nil {
-		return errors.WithStack(err1)
-	}
-	if err2 != nil {
-		return errors.WithStack(err2)
-	}
-	return nil
-}
-
-func (f *xrdFile) Read(data []byte) (int, error) {
-	n, err := f.f.ReadAt(data, f.pos)
-	f.pos += int64(n)
-	return n, err
-}
-
-func (f *xrdFile) ReadAt(data []byte, offset int64) (int, error) {
-	return f.f.ReadAt(data, offset)
-}
-
-func (f *xrdFile) Write(data []byte) (int, error) {
-	n, err := f.f.WriteAt(data, f.pos)
-	f.pos += int64(n)
-	return n, err
-}
-
-func (f *xrdFile) WriteAt(data []byte, offset int64) (int, error) {
-	return f.f.WriteAt(data, offset)
-}
-
-func (f *xrdFile) Seek(offset int64, whence int) (int64, error) {
-	var err error
-	switch whence {
-	case io.SeekStart:
-		f.pos = offset
-	case io.SeekEnd:
-		st, err := f.Stat()
-		if err != nil {
-			return 0, errors.Errorf("rootio: could not xrootd-stat %q: %v", f.Name(), err)
-		}
-		f.pos = st.Size() - offset
-	case io.SeekCurrent:
-		f.pos += offset
-	}
-	return f.pos, err
-}
-
-func (f *xrdFile) Stat() (os.FileInfo, error) {
-	v, err := f.f.Stat(context.Background())
-	return v, err
-}
-
 var (
 	_ Reader = (*tmpFile)(nil)
 	_ Writer = (*tmpFile)(nil)
 
-	_ Reader = (*xrdFile)(nil)
-	_ Writer = (*xrdFile)(nil)
+	_ Reader = (*xrdio.File)(nil)
+	_ Writer = (*xrdio.File)(nil)
 )
