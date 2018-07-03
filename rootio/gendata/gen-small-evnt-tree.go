bfabf256aede171df77282bb40e1569a98dfa9c1
Sebastien Binet <binet@cern.ch>
2018-07-03 11:10:55 +0200

    rootio/gendata: add license headers

--- rootio/gendata/gen-small-evnt-tree.go
+++ rootio/gendata/gen-small-evnt-tree.go
@@ -1,5 +1,9 @@
 // +build ignore
 
+// Copyright 2018 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
