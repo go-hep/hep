bc5d10ad31aff995a49dd2879068ec8a65da5ac1
Sebastien Binet <binet@cern.ch>
2018-07-08 21:01:01 +0200

    brio/cmd/brio-gen/internal/gen: test generation of (un)marshaling code

---
+++ brio/cmd/brio-gen/internal/gen/_test/pkg/pkg.go
@@ -0,0 +1,40 @@
+// Copyright 2018 The go-hep Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pkg
+
+import "time"
+
+type T1 struct {
+	b    bool
+	u    uint
+	u8   uint8
+	u16  uint16
+	u32  uint32
+	u64  uint64
+	i    int
+	i8   int8
+	i16  int16
+	i32  int32
+	i64  int64
+	f64  float64
+	c64  complex64
+	c128 complex128
+	str  string
+	bs   []byte
+
+	arri64 [10]int64
+	slii64 []int64
+	ptri64 *int64
+
+	t2 T2
+}
+
+type T2 struct {
+	ID int64
+}
+
+type T3 struct {
+	Time time.Time // implements encoding.Binary(Un)Marshaler
+}
