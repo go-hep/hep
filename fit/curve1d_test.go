fbbdeaff6117b4b206b7777ba230b1e8f1893367
Sebastien Binet <binet@cern.ch>
2018-07-09 11:04:23 +0200

    fit: improve coverage

--- fit/curve1d_test.go
+++ fit/curve1d_test.go
@@ -303,6 +303,71 @@ func ExampleCurve1D_powerlaw() {
 	}
 }
 
+func TestCurve1DGaussianDefaultOpt(t *testing.T) {
+	var (
+		cst   = 3.0
+		mean  = 30.0
+		sigma = 20.0
+		want  = []float64{cst, mean, sigma}
+	)
+
+	xdata, ydata, err := readXY("testdata/gauss-data.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gauss := func(x, cst, mu, sigma float64) float64 {
+		v := (x - mu)
+		return cst * math.Exp(-v*v/sigma)
+	}
+
+	res, err := fit.Curve1D(
+		fit.Func1D{
+			F: func(x float64, ps []float64) float64 {
+				return gauss(x, ps[0], ps[1], ps[2])
+			},
+			X:  xdata,
+			Y:  ydata,
+			Ps: []float64{10, 10, 10},
+		},
+		nil, nil,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := res.Status.Err(); err != nil {
+		t.Fatal(err)
+	}
+	if got := res.X; !floats.EqualApprox(got, want, 1e-3) {
+		t.Fatalf("got= %v\nwant=%v\n", got, want)
+	}
+
+	{
+		p := hplot.New()
+		p.X.Label.Text = "Gauss"
+		p.Y.Label.Text = "y-data"
+
+		s := hplot.NewS2D(hplot.ZipXY(xdata, ydata))
+		s.Color = color.RGBA{0, 0, 255, 255}
+		p.Add(s)
+
+		f := plotter.NewFunction(func(x float64) float64 {
+			return gauss(x, res.X[0], res.X[1], res.X[2])
+		})
+		f.Color = color.RGBA{255, 0, 0, 255}
+		f.Samples = 1000
+		p.Add(f)
+
+		p.Add(plotter.NewGrid())
+
+		err := p.Save(20*vg.Centimeter, -1, "testdata/gauss-plot-default-opt.png")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
 func genXY(n int, f func(x float64, ps []float64) float64, ps []float64, xmin, xmax float64) ([]float64, []float64) {
 	xdata := make([]float64, n)
 	ydata := make([]float64, n)
