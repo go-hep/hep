376a1043217adbd22813a0060095935bf0de0cc1
Mikhail Ivchenko <ematirov@gmail.com>
2018-07-25 16:47:14 +0400

    xrootd: add test for the read error in session and redirection

--- xrootd/client/session_mock_test.go
+++ xrootd/client/session_mock_test.go
@@ -10,8 +10,11 @@ import (
 	"testing"
 	"time"
 
+	"go-hep.org/x/hep/xrootd/internal/mux"
 	"go-hep.org/x/hep/xrootd/xrdproto"
 	"go-hep.org/x/hep/xrootd/xrdproto/ping"
+	"go-hep.org/x/hep/xrootd/xrdproto/signing"
+	"go-hep.org/x/hep/xrootd/xrdproto/truncate"
 )
 
 func TestSession_WaitResponse(t *testing.T) {
@@ -65,6 +68,65 @@ func TestSession_WaitResponse(t *testing.T) {
 		err := client.sessions[client.initialSessionID].Ping(context.Background())
 		if err != nil {
 			t.Fatalf("invalid ping call: %v", err)
+		}
+	}
+
+	testClientWithMockServer(serverFunc, clientFunc)
+}
+
+func TestSession_ConnectionAbort(t *testing.T) {
+	serverFunc := func(cancel func(), conn net.Conn) {
+		data, err := xrdproto.ReadRequest(conn)
+		if err != nil {
+			cancel()
+			t.Fatalf("could not read request: %v", err)
+		}
+
+		var gotRequest truncate.Request
+		gotHeader, err := unmarshalRequest(data, &gotRequest)
+		if err != nil {
+			cancel()
+			t.Fatalf("could not unmarshal request: %v", err)
+		}
+
+		err = xrdproto.WriteResponse(conn, gotHeader.StreamID, xrdproto.Ok, xrdproto.WaitResponse{Duration: time.Second})
+		if err != nil {
+			cancel()
+			t.Fatalf("could not write response: %v", err)
+		}
+	}
+	serverFuncForSecondConnection := func(cancel func(), conn net.Conn) {
+		_, err := xrdproto.ReadRequest(conn)
+		if err != nil {
+			cancel()
+			t.Fatalf("could not read request: %v", err)
+		}
+		conn.Close()
+	}
+
+	clientFunc := func(cancel func(), client *Client) {
+		p1, p2 := net.Pipe()
+		go serverFuncForSecondConnection(cancel, p2)
+		session := &session{
+			cancel:           cancel,
+			ctx:              context.Background(),
+			conn:             p1,
+			mux:              mux.New(),
+			requests:         make(map[xrdproto.StreamID]pendingRequest),
+			client:           client,
+			signRequirements: signing.Default(),
+			sessionID:        client.initialSessionID + "2",
+			isSub:            true,
+		}
+		defer session.Close()
+		defer p1.Close()
+		client.sessions[session.sessionID] = session
+		go session.consume()
+
+		f := file{sessionID: session.sessionID, fs: client.FS().(*fileSystem)}
+		err := f.Truncate(context.Background(), 0)
+		if err != nil {
+			t.Fatalf("invalid truncate call: %v", err)
 		}
 	}
 
