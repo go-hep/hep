8e48554ab7efe6dc92a88c887e6d31e5fb57b72c
Sebastien Binet <binet@cern.ch>
2018-07-26 14:19:34 +0200

    all: remove extraneous space in LICENSE blurb

--- rootio/gen-code.go
+++ rootio/gen-code.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The go-hep Authors.  All rights reserved.
+// Copyright 2017 The go-hep Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -300,7 +300,7 @@ func genH2() {
 	gofmt(f)
 }
 
-const srcHeader = `// Copyright 2017 The go-hep Authors.  All rights reserved.
+const srcHeader = `// Copyright 2017 The go-hep Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
