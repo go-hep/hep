77b926164a2352e3e231d4dd76849c7731ed590e
Sebastien Binet <binet@cern.ch>
2018-08-22 17:55:07 +0200

    rootio: introduce nowUTC

---
+++ rootio/time_test.go
@@ -0,0 +1,13 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rootio
+
+import "time"
+
+func init() {
+	nowUTC = func() time.Time {
+		return datime2time(1576331001)
+	}
+}
