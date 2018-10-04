58daf38a02a0983b50b1fcffbb4f9038b8d67cde
Sebastien Binet <binet@cern.ch>
2018-10-04 15:30:01 +0200

    xrootd/xrdproto/query: implement kXR_query request
    Updates go-hep/hep#170.

---
+++ xrootd/xrdproto/query/query.go
@@ -0,0 +1,92 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package query contains the types related to the query request.
+// See xrootd protocol specification (http://xrootd.org/doc/dev45/XRdv310.pdf, p. 79) for more details.
+package query // import "go-hep.org/x/hep/xrootd/xrdproto/query"
+
+import (
+	"go-hep.org/x/hep/xrootd/internal/xrdenc"
+	"go-hep.org/x/hep/xrootd/xrdfs"
+)
+
+// Query parameters.
+const (
+	Stats          = 1  // Query server statistics
+	Prep           = 2  // Query prepare status
+	Checksum       = 3  // Query file checksum
+	XAttr          = 4  // Query file extended attributes
+	Space          = 5  // Query server logical space statistics
+	CancelChecksum = 6  // Query file checksum cancellation
+	Config         = 7  // Query server configuration
+	Visa           = 8  // Query file visa attributes
+	Opaque1        = 16 // Query implementation-dependent information
+	Opaque2        = 32 // Query implementation-dependent information
+	Opaque3        = 64 // Query implementation-dependent information
+)
+
+// RequestID is the id of the request, it is sent as part of message.
+// See xrootd protocol specification for details: http://xrootd.org/doc/dev45/XRdv310.pdf, 2.3 Client Request Format.
+const RequestID uint16 = 3001
+
+// Request holds the query request parameters.
+type Request struct {
+	Query  uint16
+	_      [2]byte
+	Handle xrdfs.FileHandle
+	_      [8]byte
+	Args   []byte
+}
+
+// ReqID implements xrdproto.Request.ReqID.
+func (req *Request) ReqID() uint16 { return RequestID }
+
+// ShouldSign implements xrdproto.Request.ShouldSign.
+func (*Request) ShouldSign() bool { return false }
+
+// MarshalXrd implements xrdproto.Marshaler.
+func (o Request) MarshalXrd(w *xrdenc.WBuffer) error {
+	w.WriteU16(o.Query)
+	w.Next(2)
+	w.WriteBytes(o.Handle[:])
+	w.Next(8)
+	w.WriteI32(int32(len(o.Args)))
+	w.WriteBytes(o.Args)
+	return nil
+}
+
+// UnmarshalXrd implements xrdproto.Unmarshaler.
+func (o *Request) UnmarshalXrd(r *xrdenc.RBuffer) error {
+	o.Query = r.ReadU16()
+	r.Skip(2)
+	r.ReadBytes(o.Handle[:])
+	r.Skip(8)
+	n := r.ReadI32()
+	if n > 0 {
+		o.Args = make([]byte, n)
+		r.ReadBytes(o.Args)
+	}
+	return nil
+}
+
+// Response is the response issued by the server to a query request.
+type Response struct {
+	Data []byte
+}
+
+// RespID implements xrdproto.Response.RespID.
+func (*Response) RespID() uint16 { return RequestID }
+
+// MarshalXrd implements xrdproto.Marshaler.
+func (o Response) MarshalXrd(w *xrdenc.WBuffer) error {
+	w.WriteBytes(o.Data)
+	return nil
+}
+
+// UnmarshalXrd implements xrdproto.Unmarshaler.
+func (o *Response) UnmarshalXrd(r *xrdenc.RBuffer) error {
+	o.Data = make([]byte, r.Len())
+	r.ReadBytes(o.Data)
+	return nil
+}
---
+++ xrootd/xrdproto/query/query_test.go
@@ -0,0 +1,189 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package query_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+	"strings"
+	"testing"
+
+	"go-hep.org/x/hep/xrootd/client"
+	"go-hep.org/x/hep/xrootd/internal/xrdenc"
+	"go-hep.org/x/hep/xrootd/xrdfs"
+	"go-hep.org/x/hep/xrootd/xrdproto"
+	"go-hep.org/x/hep/xrootd/xrdproto/query"
+)
+
+func TestRequest(t *testing.T) {
+	for _, want := range []query.Request{
+		{
+			Query: query.Config,
+			Args:  []byte("bind_max chksum"),
+		},
+		{
+			Query:  query.Visa,
+			Handle: [4]byte{1, 2, 3, 4},
+		},
+	} {
+		t.Run("", func(t *testing.T) {
+			var (
+				err error
+				w   = new(xrdenc.WBuffer)
+				got query.Request
+			)
+
+			if want.ReqID() != query.RequestID {
+				t.Fatalf("invalid request ID: got=%d want=%d", want.ReqID(), query.RequestID)
+			}
+
+			if want.ShouldSign() {
+				t.Fatalf("invalid")
+			}
+
+			err = want.MarshalXrd(w)
+			if err != nil {
+				t.Fatalf("could not marshal request: %v", err)
+			}
+
+			r := xrdenc.NewRBuffer(w.Bytes())
+			err = got.UnmarshalXrd(r)
+			if err != nil {
+				t.Fatalf("could not unmarshal request: %v", err)
+			}
+
+			if !reflect.DeepEqual(got, want) {
+				t.Fatalf("round trip failed:\ngot = %#v\nwant= %#v\n", got, want)
+			}
+		})
+	}
+}
+
+func TestResponse(t *testing.T) {
+	for _, want := range []query.Response{
+		{
+			Data: []byte("1234"),
+		},
+		{
+			Data: []byte("oss.cgroup=public&oss.space=499337216&oss.free=444280832&oss.maxf=444280832&oss.used=55056384&oss.quota=-1\x00"),
+		},
+	} {
+		t.Run("", func(t *testing.T) {
+			var (
+				err error
+				w   = new(xrdenc.WBuffer)
+				got query.Response
+			)
+
+			if want.RespID() != query.RequestID {
+				t.Fatalf("invalid response ID: got=%d want=%d", want.RespID(), query.RequestID)
+			}
+
+			err = want.MarshalXrd(w)
+			if err != nil {
+				t.Fatalf("could not marshal response: %v", err)
+			}
+
+			r := xrdenc.NewRBuffer(w.Bytes())
+			err = got.UnmarshalXrd(r)
+			if err != nil {
+				t.Fatalf("could not unmarshal response: %v", err)
+			}
+
+			if !reflect.DeepEqual(got, want) {
+				t.Fatalf("round trip failed:\ngot = %#v\nwant= %#v\n", got, want)
+			}
+		})
+	}
+}
+
+func Example() {
+	bkg := context.Background()
+
+	cl, err := client.NewClient(bkg, "ccxrootdgotest.in2p3.fr:9001", "gopher")
+	if err != nil {
+		log.Fatalf("could not create client: %v", err)
+	}
+	defer cl.Close()
+
+	fs := cl.FS()
+	f, err := fs.Open(bkg, "/tmp/dir1/file1.txt", xrdfs.OpenModeOwnerRead, xrdfs.OpenOptionsOpenRead)
+	if err != nil {
+		log.Fatalf("open error: %v", err)
+	}
+	defer f.Close(bkg)
+
+	var (
+		resp query.Response
+		req  = query.Request{
+			Query:  query.Space,
+			Handle: f.Handle(),
+			Args:   []byte("/tmp/dir1/file1.txt"),
+		}
+	)
+
+	id, err := cl.Send(bkg, &resp, &req)
+	if err != nil {
+		log.Fatalf("space request error: %v", err)
+	}
+	fmt.Printf("sess: %s\n", id)
+	// fmt.Printf("space: %q\n", resp.Data)
+
+	cfg := []string{
+		"bind_max",
+		"chksum",
+		"cid", "cms", "pio_max",
+		"readv_ior_max",
+		"readv_iov_max",
+		"role",
+		"sitename",
+		"tpc",
+		"version",
+		"wan_port",
+		"wan_window",
+		"window",
+	}
+
+	req = query.Request{
+		Query: query.Config,
+		Args:  []byte(strings.Join(cfg, " ")),
+	}
+
+	id, err = cl.Send(bkg, &resp, &req)
+	if err != nil {
+		log.Fatalf("config request error: %v", err)
+	}
+	for i, v := range strings.Split(strings.TrimRight(string(resp.Data), "\n"), "\n") {
+		if v == cfg[i] {
+			fmt.Printf("config: %s=N/A\n", v)
+			continue
+		}
+		fmt.Printf("config: %s=%q\n", cfg[i], v)
+	}
+
+	// Output:
+	// sess: ccxrootdgotest.in2p3.fr:9001
+	// config: bind_max="15"
+	// config: chksum=N/A
+	// config: cid=N/A
+	// config: cms="none|"
+	// config: pio_max="5"
+	// config: readv_ior_max="2097136"
+	// config: readv_iov_max="1024"
+	// config: role="server"
+	// config: sitename=N/A
+	// config: tpc=N/A
+	// config: version="v4.8.4"
+	// config: wan_port=N/A
+	// config: wan_window=N/A
+	// config: window="87380"
+}
+
+var (
+	_ xrdproto.Request  = (*query.Request)(nil)
+	_ xrdproto.Response = (*query.Response)(nil)
+)
