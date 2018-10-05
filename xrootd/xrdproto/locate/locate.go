cef73bf648fdf9b184ea51faaed70483a5484dd2
Sebastien Binet <binet@cern.ch>
2018-10-05 23:34:30 +0200

    xrootd/xrdproto/locate: implement kXR_locate
    Updates go-hep/hep#170.

---
+++ xrootd/xrdproto/locate/locate.go
@@ -0,0 +1,73 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package locate contains the types related to the locate request.
+// See xrootd protocol specification (http://xrootd.org/doc/dev45/XRdv310.pdf, p. 51) for more details.
+package locate // import "go-hep.org/x/hep/xrootd/xrdproto/locate"
+
+import (
+	"go-hep.org/x/hep/xrootd/internal/xrdenc"
+)
+
+// locate options.
+const (
+	AddPeers   = 1 << 0  // AddPeers adds eligible peers to the location output
+	Refresh    = 1 << 7  // Refresh updates cached information on the file’s location
+	PreferName = 1 << 8  // PreferName indicates a hostname response is preferred
+	NoWait     = 1 << 13 // NoWait provides informations as soon as possible
+)
+
+// RequestID is the id of the request, it is sent as part of message.
+// See xrootd protocol specification for details: http://xrootd.org/doc/dev45/XRdv310.pdf, 2.3 Client Request Format.
+const RequestID uint16 = 3027
+
+// Request holds the locate request parameters.
+type Request struct {
+	Options uint16 // Options to apply when Path is opened
+	_       [14]byte
+	Path    string // Path of the file to locate
+}
+
+// ReqID implements xrdproto.Request.ReqID.
+func (req *Request) ReqID() uint16 { return RequestID }
+
+// ShouldSign implements xrdproto.Request.ShouldSign.
+func (*Request) ShouldSign() bool { return false }
+
+// MarshalXrd implements xrdproto.Marshaler.
+func (o Request) MarshalXrd(w *xrdenc.WBuffer) error {
+	w.WriteU16(o.Options)
+	w.Next(14)
+	w.WriteStr(o.Path)
+	return nil
+}
+
+// UnmarshalXrd implements xrdproto.Unmarshaler.
+func (o *Request) UnmarshalXrd(r *xrdenc.RBuffer) error {
+	o.Options = r.ReadU16()
+	r.Skip(14)
+	o.Path = r.ReadStr()
+	return nil
+}
+
+// Response is the response issued by the server to a locate request.
+type Response struct {
+	Data []byte
+}
+
+// RespID implements xrdproto.Response.RespID.
+func (*Response) RespID() uint16 { return RequestID }
+
+// MarshalXrd implements xrdproto.Marshaler.
+func (o Response) MarshalXrd(w *xrdenc.WBuffer) error {
+	w.WriteBytes(o.Data)
+	return nil
+}
+
+// UnmarshalXrd implements xrdproto.Unmarshaler.
+func (o *Response) UnmarshalXrd(r *xrdenc.RBuffer) error {
+	o.Data = make([]byte, r.Len())
+	r.ReadBytes(o.Data)
+	return nil
+}
---
+++ xrootd/xrdproto/locate/locate_test.go
@@ -0,0 +1,141 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package locate_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+	"testing"
+
+	"go-hep.org/x/hep/xrootd/client"
+	"go-hep.org/x/hep/xrootd/internal/xrdenc"
+	"go-hep.org/x/hep/xrootd/xrdproto"
+	"go-hep.org/x/hep/xrootd/xrdproto/locate"
+)
+
+func Example() {
+	bkg := context.Background()
+
+	cl, err := client.NewClient(bkg, "ccxrootdgotest.in2p3.fr:9001", "gopher")
+	if err != nil {
+		log.Fatalf("could not create client: %v", err)
+	}
+	defer cl.Close()
+
+	var (
+		resp locate.Response
+		req  = locate.Request{
+			Options: locate.PreferName | locate.Refresh | locate.AddPeers,
+			Path:    "/tmp/dir1/file1.txt",
+		}
+	)
+
+	id, err := cl.Send(bkg, &resp, &req)
+	if err != nil {
+		log.Fatalf("locate request error: %v", err)
+	}
+	fmt.Printf("sess: %s\n", id)
+	fmt.Printf("locate: %q\n", bytes.TrimRight(resp.Data, "\x00"))
+
+	// Output:
+	// sess: ccxrootdgotest.in2p3.fr:9001
+	// locate: "Sr[::127.0.0.1]:9001"
+}
+
+func TestRequest(t *testing.T) {
+	for _, want := range []locate.Request{
+		{
+			Options: locate.AddPeers,
+			Path:    "/tmp/dir1/file1.txt",
+		},
+		{
+			Options: locate.AddPeers | locate.Refresh,
+			Path:    "/tmp/dir1/file1.txt?foo=bar",
+		},
+		{
+			Options: locate.NoWait | locate.PreferName,
+			Path:    "*file1.txt?foo=bar",
+		},
+		{
+			Options: locate.NoWait | locate.PreferName,
+			Path:    "*",
+		},
+	} {
+		t.Run(want.Path, func(t *testing.T) {
+			var (
+				err error
+				w   = new(xrdenc.WBuffer)
+				got locate.Request
+			)
+
+			if want.ReqID() != locate.RequestID {
+				t.Fatalf("invalid request ID: got=%d want=%d", want.ReqID(), locate.RequestID)
+			}
+
+			if want.ShouldSign() {
+				t.Fatalf("invalid")
+			}
+
+			err = want.MarshalXrd(w)
+			if err != nil {
+				t.Fatalf("could not marshal request: %v", err)
+			}
+
+			r := xrdenc.NewRBuffer(w.Bytes())
+			err = got.UnmarshalXrd(r)
+			if err != nil {
+				t.Fatalf("could not unmarshal request: %v", err)
+			}
+
+			if !reflect.DeepEqual(got, want) {
+				t.Fatalf("round trip failed:\ngot = %#v\nwant= %#v\n", got, want)
+			}
+		})
+	}
+}
+
+func TestResponse(t *testing.T) {
+	for _, want := range []locate.Response{
+		{Data: []byte("")},
+		{Data: []byte("1234")},
+		{Data: []byte("localhost:1024")},
+		{Data: []byte("0.0.0.0:1024")},
+	} {
+		t.Run("", func(t *testing.T) {
+			var (
+				err error
+				w   = new(xrdenc.WBuffer)
+				got locate.Response
+			)
+
+			if want.RespID() != locate.RequestID {
+				t.Fatalf("invalid response ID: got=%d want=%d", want.RespID(), locate.RequestID)
+			}
+
+			err = want.MarshalXrd(w)
+			if err != nil {
+				t.Fatalf("could not marshal response: %v", err)
+			}
+
+			r := xrdenc.NewRBuffer(w.Bytes())
+			err = got.UnmarshalXrd(r)
+			if err != nil {
+				t.Fatalf("could not unmarshal response: %v", err)
+			}
+
+			if !reflect.DeepEqual(got, want) {
+				t.Fatalf("round trip failed:\ngot = %#v\nwant= %#v\n", got, want)
+			}
+		})
+	}
+}
+
+var (
+	_ xrdproto.Request  = (*locate.Request)(nil)
+	_ xrdproto.Response = (*locate.Response)(nil)
+)
