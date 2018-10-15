0210ec43c24bca8605cb2d3746bd9723ae5ba423
Sebastien Binet <binet@cern.ch>
2018-10-15 12:32:47 +0200

    rio: add import spec

--- rio/rio.go
+++ rio/rio.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package rio
+package rio // import "go-hep.org/x/hep/rio"
 
 import (
 	"bytes"
