d6caa268ffea287c7fd11553ba8cf8fa6d2d6667
Sebastien Binet <binet@cern.ch>
2018-11-08 18:18:57 +0100

    groot/cmd/root-srv: bump to jsTree-3.3.7

--- groot/cmd/root-srv/server/server.go
+++ groot/cmd/root-srv/server/server.go
@@ -236,8 +236,8 @@ const page = `<html>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/themes/default/style.min.css" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.3/jstree.min.js"></script>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jstree/3.3.7/themes/default/style.min.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.7/jstree.min.js"></script>
 	<style>
 	input[type=file] {
 		display: none;
