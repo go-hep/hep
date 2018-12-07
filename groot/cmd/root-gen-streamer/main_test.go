760d4cc73bc39c0ab738d55a93f6e08d39d08cc2
Sebastien Binet <binet@cern.ch>
2018-12-07 15:45:49 +0100

    groot/cmd/root-gen-streamer: add test

---
+++ groot/cmd/root-gen-streamer/main_test.go
@@ -0,0 +1,97 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"go-hep.org/x/hep/groot/internal/rdatatest" // make sure this is compiled
+)
+
+var evt rdatatest.Event
+
+func TestGenerate(t *testing.T) {
+
+	for _, tc := range []struct {
+		pkg   string
+		types []string
+		want  string
+	}{
+		{
+			pkg:   "image",
+			types: []string{"Point"},
+			want:  "testdata/image.txt",
+		},
+		{
+			pkg:   "go-hep.org/x/hep/groot/internal/rdatatest",
+			types: []string{"Particle", "HLV", "Event"},
+			want:  "testdata/rdatatest.txt",
+		},
+	} {
+		t.Run(tc.pkg, func(t *testing.T) {
+			buf := new(bytes.Buffer)
+			err := generate(buf, tc.pkg, tc.types)
+			if err != nil {
+				t.Fatalf("could not generate streamer: %v", err)
+			}
+			want, err := ioutil.ReadFile(tc.want)
+			if err != nil {
+				t.Fatalf("could not read reference streamer: %v", err)
+			}
+
+			if got, want := buf.String(), string(want); got != want {
+				t.Fatalf("error:\n%s\n", diff(t, got, want))
+			}
+		})
+	}
+}
+
+func diff(t *testing.T, chk, ref string) string {
+	t.Helper()
+
+	if !hasDiffCmd {
+		return fmt.Sprintf("=== got ===\n%s\n=== want ===\n%s\n", chk, ref)
+	}
+
+	tmpdir, err := ioutil.TempDir("", "groot-diff-")
+	if err != nil {
+		t.Fatalf("could not create tmpdir: %v", err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	got := filepath.Join(tmpdir, "got.txt")
+	err = ioutil.WriteFile(got, []byte(chk), 0644)
+	if err != nil {
+		t.Fatalf("could not create %s file: %v", got, err)
+	}
+
+	want := filepath.Join(tmpdir, "want.txt")
+	err = ioutil.WriteFile(want, []byte(ref), 0644)
+	if err != nil {
+		t.Fatalf("could not create %s file: %v", want, err)
+	}
+
+	out := new(bytes.Buffer)
+	cmd := exec.Command("diff", "-urN", want, got)
+	cmd.Stdout = out
+	cmd.Stderr = out
+	err = cmd.Run()
+	return out.String() + "\nerror: " + err.Error()
+}
+
+var hasDiffCmd = false
+
+func init() {
+	_, err := exec.LookPath("diff")
+	if err == nil {
+		hasDiffCmd = true
+	}
+}
---
+++ groot/internal/rdatatest/event.go
@@ -0,0 +1,94 @@
+// Copyright 2018 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rdatatest
+
+import (
+	"go-hep.org/x/hep/groot/rbase"
+	"go-hep.org/x/hep/groot/rcont"
+	"go-hep.org/x/hep/groot/root"
+)
+
+// Event is a simple type to exercize streamers generation.
+type Event struct {
+	Name string
+	u8   uint8
+	u16  uint16
+	u32  uint32
+	u64  uint64 `groot:"U64"`
+	i8   int8
+	i16  int16
+	i32  int32
+	i64  int64
+	f32  float32
+	f64  float64
+
+	b  bool
+	bb byte
+
+	u8s  []uint8
+	u16s []uint16
+	u32s []uint32
+	u64s []uint64
+	i8s  []int8
+	i16s []int16
+	i32s []int32
+	i64s []int64
+	f32s []float32
+	f64s []float64
+	bs   []bool
+	bbs  []byte
+
+	arru8s  [10]uint8
+	arru16s [10]uint16
+	arru32s [10]uint32
+	arru64s [10]uint64
+	arri8s  [10]int8
+	arri16s [10]int16
+	arri32s [10]int32
+	arri64s [10]int64
+	arrf32s [10]float32
+	arrf64s [10]float64
+	arrbs   [10]bool
+	arrbbs  [10]byte
+
+	SliF64 []float64   `groot:"SliceF64"`
+	SliStr []string    `groot:"SliceStr"`
+	SliHLV []HLV       `groot:"SliceHLV"`
+	ArrF64 [10]float64 `groot:"ArrF64"`
+}
+
+// Particle is a simple type to exercize streamers generation.
+type Particle struct {
+	name string
+	pid  int
+	mom  HLV
+}
+
+// HLV is a simple type to exercize streamers generation.
+type HLV struct {
+	px, py, pz, e float64
+}
+
+// FIXME(sbinet)
+//  - support types that "inherit" from TObject
+//  - support types that contain a TList
+//  - support types that contain a pointer to TObject
+//  - support types that contain a pointer to any-object
+
+type TObject struct {
+	rbase.Object
+	name string
+}
+
+type TList struct {
+	rbase.Object
+	objs []root.Object
+	list rcont.List
+}
+
+type TClonesArray struct {
+	rbase.Object
+	//	clones rcont.ClonesArray // FIXME(sbinet)
+}
