9f42e79e1f225b7f581d816f36c5505af94a2b92
Sebastien Binet <binet@cern.ch>
2018-12-13 12:17:32 +0100

    groot/{cmd/root-gen-streamer,internal/rdatatest}: test full generate+compile+run cycle for streamers

--- groot/cmd/root-gen-streamer/main_test.go
+++ groot/cmd/root-gen-streamer/main_test.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"go-hep.org/x/hep/groot/internal/rdatatest" // make sure this is compiled
@@ -47,6 +48,160 @@ func TestGenerate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestGenerateCompileRun(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		pkg   string
+		types []string
+		out   string
+		tmpl  string
+		want  string
+	}{
+		{
+			name:  "builtins",
+			pkg:   "go-hep.org/x/hep/groot/internal/rdatatest",
+			types: []string{"Builtins"},
+			out:   "../../internal/rdatatest/pkg_gen.go",
+			tmpl:  "NewBuiltins",
+			want: `>>> file[testdata/out.root]
+key[000]: data;1 "" (go_hep_org::x::hep::groot::internal::rdatatest::Builtins) => &{true 8 16 32 64 -8 -16 -32 -64 32.32 64.64 builtins}
+`,
+		},
+		{
+			name:  "arr-builtins",
+			pkg:   "go-hep.org/x/hep/groot/internal/rdatatest",
+			types: []string{"ArrBuiltins"},
+			out:   "../../internal/rdatatest/pkg_gen.go",
+			tmpl:  "NewArrBuiltins",
+			want: `>>> file[testdata/out.root]
+key[000]: data;1 "" (go_hep_org::x::hep::groot::internal::rdatatest::ArrBuiltins) => &{[true false] [8 88] [16 1616] [32 3232] [64 6464] [-8 -88] [-16 -1616] [-32 -3232] [-64 -6464] [32.32 -32.32] [64.64 64.64]}
+`,
+		},
+		{
+			name:  "struct-t1",
+			pkg:   "go-hep.org/x/hep/groot/internal/rdatatest",
+			types: []string{"HLV", "T1"}, // FIXME(sbinet): only select T1 and let root-gen-streamer pick-up HLV
+			out:   "../../internal/rdatatest/pkg_gen.go",
+			tmpl:  "NewT1",
+			want: `>>> file[testdata/out.root]
+key[000]: data;1 "" (go_hep_org::x::hep::groot::internal::rdatatest::T1) => &{hello {1 2 3 4}}
+`,
+		},
+		//		{
+		//			name:  "struct-t2",
+		//			pkg:   "go-hep.org/x/hep/groot/internal/rdatatest",
+		//			types: []string{"HLV", "T2"}, // FIXME(sbinet): only select T2 and let root-gen-streamer pick-up HLV
+		//			out:   "../../internal/rdatatest/pkg_gen.go",
+		//			tmpl:  "NewT2",
+		//			want: `>>> file[testdata/out.root]
+		//key[000]: data;1 "" (go_hep_org::x::hep::groot::internal::rdatatest::T1) => &{hello {1 2 3 4}}
+		//`,
+		//		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			os.Remove(tc.out)
+			os.Remove("testdata/run.go")
+			defer os.Remove(tc.out)
+			defer os.Remove("testdata/run.go")
+			defer os.Remove("testdata/out.root")
+
+			var (
+				out = new(bytes.Buffer)
+				err error
+				cmd *exec.Cmd
+			)
+
+			out.Reset()
+			cmd = exec.Command("go", "get", "-v", tc.pkg)
+			cmd.Stdout = out
+			cmd.Stderr = out
+			err = cmd.Run()
+			if err != nil {
+				t.Fatalf("could not compile package with streamer data:\n%v\nerr: %v", out.String(), err)
+			}
+
+			out.Reset()
+			cmd = exec.Command(
+				"root-gen-streamer",
+				"-p", tc.pkg, "-t", strings.Join(tc.types, ","),
+				"-o", tc.out,
+			)
+			cmd.Stdout = out
+			cmd.Stderr = out
+			err = cmd.Run()
+			if err != nil {
+				t.Fatalf("could not generate streamer data:\n%v\nerr: %v", out.String(), err)
+			}
+
+			out.Reset()
+			cmd = exec.Command("go", "get", "-v", tc.pkg)
+			cmd.Stdout = out
+			cmd.Stderr = out
+			err = cmd.Run()
+			if err != nil {
+				t.Fatalf("could not recompile package with streamer data:\n%v\nerr: %v", out.String(), err)
+			}
+
+			err = ioutil.WriteFile("testdata/run.go", []byte(fmt.Sprintf(`// +build ignore
+package main
+
+import (
+	"log"
+
+	"go-hep.org/x/hep/groot"
+	"go-hep.org/x/hep/groot/internal/rdatatest"
+)
+
+func main() {
+	f, err := groot.Create("testdata/out.root")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	v := rdatatest.%s()
+	err = f.Put("data", v)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+`, tc.tmpl,
+			)), 0644)
+			if err != nil {
+				t.Fatalf("could not generate test-write program: %v", err)
+			}
+
+			out.Reset()
+			cmd = exec.Command("go", "run", "testdata/run.go")
+			cmd.Stdout = out
+			cmd.Stderr = out
+			err = cmd.Run()
+			if err != nil {
+				t.Fatalf("could not run test-write program:\n%v\nerr: %v\n", out.String(), err)
+			}
+
+			out.Reset()
+			cmd = exec.Command("root-dump", "testdata/out.root")
+			cmd.Stdout = out
+			cmd.Stderr = out
+			err = cmd.Run()
+			if err != nil {
+				t.Fatalf("could not run root-dump:\n%v\nerr: %v", out.String(), err)
+			}
+
+			if got, want := out.String(), tc.want; got != want {
+				t.Fatalf("error:\n%v", diff(t, got, want))
+			}
+		})
+	}
+
 }
 
 func diff(t *testing.T, chk, ref string) string {
--- groot/internal/rdatatest/event.go
+++ groot/internal/rdatatest/event.go
@@ -6,17 +6,18 @@ package rdatatest
 
 import (
 	"go-hep.org/x/hep/groot/rbase"
+	"go-hep.org/x/hep/groot/rbytes"
 	"go-hep.org/x/hep/groot/rcont"
 	"go-hep.org/x/hep/groot/root"
 )
 
 // Event is a simple type to exercize streamers generation.
 type Event struct {
-	Name string
+	name string `groot:"Name"`
 	u8   uint8
 	u16  uint16
 	u32  uint32
-	u64  uint64 `groot:"U64"`
+	u64  uint64
 	i8   int8
 	i16  int16
 	i32  int32
@@ -59,7 +60,8 @@ type Event struct {
 	ArrF64 [10]float64 `groot:"ArrF64"`
 }
 
-func (Event) RVersion() int16 { return 1 }
+func (*Event) RVersion() int16 { return 1 }
+func (*Event) Class() string   { return "go-hep.org/x/hep/groot/internal/rdatatest.Event" }
 
 // Particle is a simple type to exercize streamers generation.
 type Particle struct {
@@ -68,14 +70,148 @@ type Particle struct {
 	mom  HLV
 }
 
-func (Particle) RVersion() int16 { return 1 }
+func (*Particle) RVersion() int16 { return 1 }
+func (*Particle) Class() string   { return "go-hep.org/x/hep/groot/internal/rdatatest.Particle" }
 
 // HLV is a simple type to exercize streamers generation.
 type HLV struct {
 	px, py, pz, e float64
 }
 
-func (HLV) RVersion() int16 { return 1 }
+func (*HLV) RVersion() int16 { return 1 }
+func (*HLV) Class() string   { return "go-hep.org/x/hep/groot/internal/rdatatest.HLV" }
+
+// Builtins exercizes all simple builtins.
+type Builtins struct {
+	b bool
+
+	u8  uint8
+	u16 uint16
+	u32 uint32
+	u64 uint64
+
+	i8  int8
+	i16 int16
+	i32 int32
+	i64 int64
+
+	f32 float32
+	f64 float64
+
+	// c64  complex64  // FIXME(sbinet)
+	// c128 complex128 // FIXME(sbinet)
+
+	name string `groot:"Name"`
+}
+
+func (*Builtins) RVersion() int16 { return 1 }
+func (*Builtins) Class() string   { return "go-hep.org/x/hep/groot/internal/rdatatest.Builtins" }
+
+func NewBuiltins() *Builtins {
+	return &Builtins{
+		b: true,
+
+		u8:  8,
+		u16: 16,
+		u32: 32,
+		u64: 64,
+
+		i8:  -8,
+		i16: -16,
+		i32: -32,
+		i64: -64,
+
+		f32: 32.32,
+		f64: 64.64,
+
+		// c64:  complex(float32(-1), float32(+2)), // FIXME(sbinet)
+		// c128: complex(-2, +3),                   // FIXME(sbinet)
+
+		name: "builtins",
+	}
+}
+
+// ArrBuiltins exercizes all arrays of simple builtins.
+type ArrBuiltins struct {
+	b [2]bool
+
+	u8  [2]uint8
+	u16 [2]uint16
+	u32 [2]uint32
+	u64 [2]uint64
+
+	i8  [2]int8
+	i16 [2]int16
+	i32 [2]int32
+	i64 [2]int64
+
+	f32 [2]float32
+	f64 [2]float64
+
+	// c64  [2]complex64  // FIXME(sbinet)
+	// c128 [2]complex128 // FIXME(sbinet)
+
+	// name [2]string `groot:"Name"` // FIXME(sbinet)
+}
+
+func (*ArrBuiltins) RVersion() int16 { return 1 }
+func (*ArrBuiltins) Class() string   { return "go-hep.org/x/hep/groot/internal/rdatatest.ArrBuiltins" }
+
+func NewArrBuiltins() *ArrBuiltins {
+	return &ArrBuiltins{
+		b: [2]bool{true, false},
+
+		u8:  [2]uint8{8, 88},
+		u16: [2]uint16{16, 1616},
+		u32: [2]uint32{32, 3232},
+		u64: [2]uint64{64, 6464},
+
+		i8:  [2]int8{-8, -88},
+		i16: [2]int16{-16, -1616},
+		i32: [2]int32{-32, -3232},
+		i64: [2]int64{-64, -6464},
+
+		f32: [2]float32{32.32, -32.32},
+		f64: [2]float64{64.64, +64.64},
+
+		// c64:  complex(float32(-1), float32(+2)), // FIXME(sbinet)
+		// c128: complex(-2, +3),                   // FIXME(sbinet)
+
+		// name: [2]string{"builtins", "arrays"}, // FIXME(sbinet)
+	}
+}
+
+// T1 exercizes a user type containing another user-type.
+type T1 struct {
+	name string `groot:"Name"`
+	hlv  HLV    `groot:"MyHLV"`
+}
+
+func (*T1) RVersion() int16 { return 1 }
+func (*T1) Class() string   { return "go-hep.org/x/hep/groot/internal/rdatatest.T1" }
+
+func NewT1() *T1 {
+	return &T1{
+		name: "hello",
+		hlv:  HLV{1, 2, 3, 4},
+	}
+}
+
+// T2 exercizes a user type containing an array of another user-type.
+type T2 struct {
+	name string `groot:"Name"`
+	hlvs [2]HLV `groot:"MyHLVs"`
+}
+
+func (*T2) RVersion() int16 { return 1 }
+func (*T2) Class() string   { return "go-hep.org/x/hep/groot/internal/rdatatest.T2" }
+
+func NewT2() *T2 {
+	return &T2{
+		name: "hello",
+		hlvs: [2]HLV{{1, 2, 3, 4}, {-1, -2, -3, -4}},
+	}
+}
 
 // FIXME(sbinet)
 //  - support types that "inherit" from TObject
@@ -98,3 +234,14 @@ type TClonesArray struct {
 	rbase.Object
 	//	clones rcont.ClonesArray // FIXME(sbinet)
 }
+
+var (
+	_ rbytes.RVersioner = (*Event)(nil)
+	_ rbytes.RVersioner = (*HLV)(nil)
+	_ rbytes.RVersioner = (*Particle)(nil)
+
+	_ rbytes.RVersioner = (*Builtins)(nil)
+	_ rbytes.RVersioner = (*ArrBuiltins)(nil)
+	_ rbytes.RVersioner = (*T1)(nil)
+	_ rbytes.RVersioner = (*T2)(nil)
+)
