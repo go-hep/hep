ac4885bbcf09150d3048609672e0b5a53afa1a8c
Sebastien Binet <binet@cern.ch>
2019-11-22 09:23:26 +0100

    ci: add binary ROOT installation scripts

---
+++ ci/build-cxx-root.go
@@ -0,0 +1,145 @@
+// Copyright 2019 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+// Command to build a given ROOT version from sources.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+func main() {
+	log.SetPrefix("")
+	log.SetFlags(0)
+
+	rvers := flag.String("root-version", "6.18.04", "ROOT version to build")
+	nproc := flag.Int("j", runtime.NumCPU(), "number of parallel build processes")
+
+	flag.Parse()
+
+	build(*rvers, *nproc)
+}
+
+func build(rvers string, nproc int) {
+	tmp, err := ioutil.TempDir("", "build-root-")
+	if err != nil {
+		log.Fatalf("could not create top-level tmp dir: %+v", err)
+	}
+	defer os.RemoveAll(tmp)
+
+	bdir := filepath.Join(tmp, "build")
+	err = os.MkdirAll(bdir, 0755)
+	if err != nil {
+		log.Fatalf("could not create build dir %q: %+v", bdir, err)
+	}
+
+	dst := filepath.Join(tmp, "root-"+rvers)
+	err = os.MkdirAll(dst, 0755)
+	if err != nil {
+		log.Fatalf("could not create dst dir %q: %+v", dst, err)
+	}
+
+	fname := filepath.Join(tmp, "build.sh")
+	err = ioutil.WriteFile(fname, []byte(fmt.Sprintf(docker, rvers, nproc)), 0644)
+	if err != nil {
+		log.Fatalf("could not create build-script: %+v", err)
+	}
+
+	cmd := exec.Command("docker", "run", "--rm",
+		"-v", fname+":/build.sh",
+		"-v", bdir+":/build/src",
+		"-v", dst+":/build/install",
+		"ubuntu:bionic", "/bin/sh", "/build.sh",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalf("could not build ROOT-%s: %+v", rvers, err)
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("could not get pwd: %+v", err)
+	}
+
+	cmd = exec.Command("tar", "zcf", filepath.Join(wd, "root-"+rvers+"-linux_amd64.tar.gz"), "root-"+rvers)
+	cmd.Dir = tmp
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalf("could not tar ROOT-%s: %+v", rvers, err)
+	}
+}
+
+const docker = `#!/bin/sh
+
+set -e
+set -x
+
+apt-get update -y
+apt-get install -y -q \
+		cmake curl \
+		g++ git \
+		python \
+		;
+
+export ROOT_VERSION="%[1]s"
+
+cd /tmp
+
+curl -O -L https://root.cern.ch/download/root_v${ROOT_VERSION}.source.tar.gz
+tar zxf ./root_v${ROOT_VERSION}.source.tar.gz
+
+cd /build/src
+cmake /tmp/root-${ROOT_VERSION} \
+ -DCMAKE_INSTALL_PREFIX=/build/install -DCMAKE_BUILD_TYPE=Release \
+ -Dall=OFF -Dfail-on-missing=OFF \
+ -Dalien=OFF \
+ -Dastiff=OFF \
+ -Dbonjour=OFF \
+ -Dbuiltin_afterimage=OFF \
+ -Dbuiltin_ftgl=OFF \
+ -Dbuiltin_glez=OFF \
+ -Dcastor=OFF \
+ -Dclad=OFF \
+ -Dchirp=OFF \
+ -Ddcache=OFF \
+ -Dfftw3=OFF \
+ -Dfitsio=OFF \
+ -Dgenvector=OFF \
+ -Dgfal=OFF \
+ -Dglite=OFF \
+ -Dgnuinstall=OFF \
+ -Dgraphics=OFF \
+ -Dgviz=OFF \
+ -Dhdfs=OFF \
+ -Dkrb5=OFF \
+ -Dldap=OFF \
+ -Dmathmore=OFF \
+ -Dmonalisa=OFF \
+ -Dmysql=OFF \
+ -Dodbc=OFF \
+ -Dpython=OFF \
+ -Dshared=OFF \
+ -Dtmva=OFF \
+ -Dvdt=OFF \
+ -Dx11=OFF \
+;
+
+make -j%[2]d
+make -j%[2]d install
+`
---
+++ ci/install-cxx-root.go
@@ -0,0 +1,100 @@
+// Copyright 2019 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+// Command to install a given (binary) ROOT version.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	log.SetPrefix("")
+	log.SetFlags(0)
+
+	rvers := flag.String("root-version", "6.18.04", "ROOT version to install")
+	odir := flag.String("o", "", "install directory for ROOT")
+
+	flag.Parse()
+
+	if *odir == "" {
+		*odir = fmt.Sprintf("root-%s", *rvers)
+	}
+
+	dst, err := filepath.Abs(*odir)
+	if err != nil {
+		log.Fatalf("could not get absolute path to %q: %+v", *odir, err)
+	}
+	switch _, err := os.Stat(filepath.Join(dst, "root-"+*rvers, "bin", "root.exe")); err {
+	case nil:
+		log.Printf("ROOT version %s already installed", *rvers)
+		return
+	default:
+		err = os.MkdirAll(dst, 0755)
+		if err != nil {
+			log.Fatalf("could not create directory %q: %+v", dst, err)
+		}
+	}
+
+	install(*rvers, dst)
+}
+
+func install(rvers, odir string) {
+	log.Printf("installing ROOT %s to: %q...", rvers, odir)
+	tmp, err := ioutil.TempDir("", "go-hep-build-")
+	if err != nil {
+		log.Fatalf("could not create top-level tmp dir: %+v", err)
+	}
+	defer os.RemoveAll(tmp)
+
+	oname := filepath.Join(tmp, "root.tar.gz")
+	targz, err := os.Create(oname)
+	if err != nil {
+		log.Fatalf("could not create ROOT archive destination file %q: %+v", oname, err)
+	}
+	defer targz.Close()
+
+	binURL := fmt.Sprintf("https://cern.ch/binet/go-hep/cern-root/root-%s-linux_amd64.tar.gz", rvers)
+	log.Printf("downloading %q...", binURL)
+	resp, err := http.Get(binURL)
+	if err != nil {
+		log.Fatalf("could not get ROOT %s from %v: %+v", rvers, binURL, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("could not get ROOT %s from %v: %v", rvers, binURL, resp.Status)
+	}
+
+	_, err = io.Copy(targz, resp.Body)
+	if err != nil {
+		log.Fatalf("could not download ROOT archive from %q: %+v", binURL, err)
+	}
+
+	err = targz.Close()
+	if err != nil {
+		log.Fatalf("could not close ROOT binary archive %q: %+v", oname, err)
+	}
+
+	log.Printf("decompressing...")
+	cmd := exec.Command("tar", "xf", oname)
+	cmd.Dir = odir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalf("could not untar ROOT binary archive: %+v", err)
+	}
+}
