c29e749d49f986c9c80ca4d7ba8da754480294a7
Sebastien Binet <binet@cern.ch>
2020-03-03 15:52:19 +0100

    rootio: removal
    Users are supposed to use go-hep.org/x/hep/groot

--- rootio/internal/rstreamers/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 The go-hep Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package rstreamers provides the StreamerInfo definitions for a bunch of
-// core ROOT classes, to setup the bootstrap procedure of being able to create
-// streamers from the Go side.
-package rstreamers
