d1be5bcf7990b9cf2ff447d7974fe872be8c7ec0
Sebastien Binet <binet@cern.ch>
2020-03-26 22:23:06 +0100

    hbook/ntup/ntroot: fix doc-package typo

--- hbook/ntup/ntroot/ntroot.go
+++ hbook/ntup/ntroot/ntroot.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package ntroot provides convenience fnctions to access ROOT trees as n-tuple
+// Package ntroot provides convenience functions to access ROOT trees as n-tuple
 // data.
 //
 // Example:
