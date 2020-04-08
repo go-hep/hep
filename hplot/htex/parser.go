c2da4cca9e5986cd421c9170a1d177f45e34efb4
Sebastien Binet <binet@cern.ch>
2020-04-08 16:39:45 +0200

    hplot/htex: first stab at parser

---
+++ hplot/htex/parser.go
@@ -0,0 +1,11 @@
+// Copyright ©2020 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package htex
+
+import "io"
+
+type parser struct {
+	r io.Reader
+}
