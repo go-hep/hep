d62708a8fdf569c8fe7a2264530ef676058a5b36
Sebastien Binet <binet@cern.ch>
2020-04-09 10:26:07 +0200

    hplot/htex: run pdflatex compilation in a temporary directory
    Fixes go-hep/hep#630.

--- hplot/htex/latex.go
+++ hplot/htex/latex.go
@@ -7,8 +7,11 @@ package htex // import "go-hep.org/x/hep/hplot/htex"
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 	"os/exec"
-	"path/filepath"
+	"path"
 )
 
 // Handler is the interface that handles the generation of PDFs
@@ -39,10 +42,16 @@ func NewHandler(cmd string) Handler {
 }
 
 func (pdf *pdfLatex) CompileLatex(fname string) error {
+	tmp, err := ioutil.TempDir("", "hplot-htex-")
+	if err != nil {
+		return fmt.Errorf("htex: could not create tmp dir: %w", err)
+	}
+	defer os.RemoveAll(tmp)
+
 	var (
 		stdout = new(bytes.Buffer)
 		args   = []string{
-			fmt.Sprintf("-output-directory=%s", filepath.Dir(fname)),
+			fmt.Sprintf("-output-directory=%s", tmp),
 			fname,
 		}
 	)
@@ -51,13 +60,36 @@ func (pdf *pdfLatex) CompileLatex(fname string) error {
 	cmd.Stdout = stdout
 	cmd.Stderr = stdout
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return fmt.Errorf(
-			"hplot: could not generate PDF from vgtex:\n%s\nerror: %w",
+			"htex: could not generate PDF from vgtex:\n%s\nerror: %w",
 			stdout.Bytes(),
 			err,
 		)
+	}
+
+	oname := fname[:len(fname)-len(".tex")] + ".pdf"
+	o, err := os.Create(oname)
+	if err != nil {
+		return fmt.Errorf("htex: could not create output PDF file: %w", err)
+	}
+	defer o.Close()
+
+	f, err := os.Open(path.Join(tmp, path.Base(oname)))
+	if err != nil {
+		return fmt.Errorf("htex: could not open generated PDF file: %w", err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(o, f)
+	if err != nil {
+		return fmt.Errorf("htex: could not copy PDF file: %w", err)
+	}
+
+	err = o.Close()
+	if err != nil {
+		return fmt.Errorf("htex: could not close PDF file: %w", err)
 	}
 
 	return nil
