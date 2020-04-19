cfed7928bb92ae7e71f161d6e3d0d205de6cbee2
rmadar <romain.madar@gmail.com>
2020-04-19 15:26:31 +0200

    Adding the first version of an example file

---
+++ sliceop/f64s/example_f64s_test.go
@@ -0,0 +1,39 @@
+// Copyright ©2020 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package f64s_test
+
+import (
+	"fmt"
+)
+
+// An example of slice filtering
+func ExampleFilter() {
+	slice := []float64{1, 2, -99, 4, 5, -99, 7}
+	condition := func(x float64) bool { return x > 0 }
+	fmt.Println(f64s.Filter(nil, slice, condition))
+
+	// Output:
+	// [1 2 4 5 7]
+}
+
+// An example of slice mapping
+func ExampleFind() {
+	slice := []float64{1, 2, -99, 4, 5, -99, 7}
+	operation := func(x float64) float64 { return x*x }
+	fmt.Println(f64s.Map(nil, slice, operation))
+
+	// Output:
+	// [1, 4, 9801, 16, 25, 9801, 49]
+}
+
+// An example of slice finding
+func ExampleFind() {
+	slice := []float64{1, 2, -99, 4, 5, -99, 7}
+	condition := func(x float64) bool { return x == -99 }
+	fmt.Println(f64s.Find(nil, slice, condition))
+
+	// Output:
+	// [2 5]
+}
