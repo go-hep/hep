32359b38464c3f6c101cc22c8d4519dc3cc4d683
rmadar <romain.madar@gmail.com>
2020-04-19 13:28:08 +0200

    Minor udpate of the documentation

--- sliceop/f64s/f64s.go
+++ sliceop/f64s/f64s.go
@@ -6,7 +6,7 @@
 package f64s
 
 
-// Return a slice with element passing the conditiond defined by f
+// Return a slice with all elements for which f(x) returns true
 func Filter(dst, src []float64, f func(v float64) bool) []float64 {
 	if dst == nil {
 		dst = []float64{}
@@ -19,7 +19,7 @@ func Filter(dst, src []float64, f func(v float64) bool) []float64 {
 	return dst	
 }
 
-// Return a slice in which the function f is applied element-wise
+// Return a slice of {f(x_i)} when src = {x_i}
 func Map(dst, src []float64, f func(v float64) float64) []float64 {
 	if dst == nil {
 		dst = []float64{}
@@ -30,7 +30,7 @@ func Map(dst, src []float64, f func(v float64) float64) []float64 {
 	return dst
 }
 
-// Return a slice of all indices corresponding to element for which f(x) is true
+// Return a slice of all indices corresponding to elements for which f(x) is true
 func Find(dst []int, src []float64, f func(v float64) bool) []int {
 	if dst == nil {
 		dst = []int{}
