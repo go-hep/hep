c79dd9bde42fbd97a82285a5318b95215a762d75
Sebastien Binet <binet@cern.ch>
2020-05-04 12:00:32 +0200

    groot/rtree: introduce FormulaFunc
    This CL introduces a new way to create Formulae: FormulaFunc.
    here is the comparison wrt Formula:
    
    $> benchstat ref.txt new.txt
    name               old time/op    new time/op    delta
    Formula/f1/Eval-4     348ns ± 3%     320ns ± 2%   -8.02%  (p=0.000 n=18+19)
    Formula/f1/Func-4     349ns ± 3%     351ns ± 2%   +0.65%  (p=0.023 n=20+19)
    Formula/f2/Eval-4     374ns ± 3%     319ns ± 1%  -14.75%  (p=0.000 n=19+18)
    Formula/f2/Func-4     370ns ± 3%     350ns ± 1%   -5.38%  (p=0.000 n=19+19)
    Formula/f3/Eval-4     373ns ± 4%     322ns ± 2%  -13.69%  (p=0.000 n=19+20)
    Formula/f3/Func-4     369ns ± 2%     352ns ± 1%   -4.67%  (p=0.000 n=19+17)
    
    name               old alloc/op   new alloc/op   delta
    Formula/f1/Eval-4     72.0B ± 0%     48.0B ± 0%  -33.33%  (p=0.000 n=20+20)
    Formula/f1/Func-4     64.0B ± 0%     48.0B ± 0%  -25.00%  (p=0.000 n=20+20)
    Formula/f2/Eval-4     72.0B ± 0%     48.0B ± 0%  -33.33%  (p=0.000 n=20+20)
    Formula/f2/Func-4     64.0B ± 0%     48.0B ± 0%  -25.00%  (p=0.000 n=20+20)
    Formula/f3/Eval-4     72.0B ± 0%     48.0B ± 0%  -33.33%  (p=0.000 n=20+20)
    Formula/f3/Func-4     64.0B ± 0%     48.0B ± 0%  -25.00%  (p=0.000 n=20+20)
    
    name               old allocs/op  new allocs/op  delta
    Formula/f1/Eval-4      3.00 ± 0%      2.00 ± 0%  -33.33%  (p=0.000 n=20+20)
    Formula/f1/Func-4      2.00 ± 0%      2.00 ± 0%     ~     (all equal)
    Formula/f2/Eval-4      3.00 ± 0%      2.00 ± 0%  -33.33%  (p=0.000 n=20+20)
    Formula/f2/Func-4      2.00 ± 0%      2.00 ± 0%     ~     (all equal)
    Formula/f3/Eval-4      3.00 ± 0%      2.00 ± 0%  -33.33%  (p=0.000 n=20+20)
    Formula/f3/Func-4      2.00 ± 0%      2.00 ± 0%     ~     (all equal)
    
    all stats:
    
    name                            time/op
    Formula/42.0/Eval-4             348ns ± 3%
    Formula/42.0/Func-4             349ns ± 3%
    Formula/F64/Eval-4              374ns ± 3%
    Formula/F64/Func-4              370ns ± 3%
    Formula/2*F64/Eval-4            373ns ± 4%
    Formula/2*F64/Func-4            369ns ± 2%
    Formula/math.Abs(2*F64)/Eval-4  385ns ± 2%
    Formula/math.Abs(2*F64)/Func-4  385ns ± 3%
    FormulaFunc/f0/Eval-4           248ns ± 2%
    FormulaFunc/f0/Func-4           281ns ± 2%
    FormulaFunc/f1/Eval-4           320ns ± 2%
    FormulaFunc/f1/Func-4           351ns ± 2%
    FormulaFunc/f2/Eval-4           319ns ± 1%
    FormulaFunc/f2/Func-4           350ns ± 1%
    FormulaFunc/f3/Eval-4           322ns ± 2%
    FormulaFunc/f3/Func-4           352ns ± 1%
    
    name                            alloc/op
    Formula/42.0/Eval-4             72.0B ± 0%
    Formula/42.0/Func-4             64.0B ± 0%
    Formula/F64/Eval-4              72.0B ± 0%
    Formula/F64/Func-4              64.0B ± 0%
    Formula/2*F64/Eval-4            72.0B ± 0%
    Formula/2*F64/Func-4            64.0B ± 0%
    Formula/math.Abs(2*F64)/Eval-4  72.0B ± 0%
    Formula/math.Abs(2*F64)/Func-4  64.0B ± 0%
    FormulaFunc/f0/Eval-4           40.0B ± 0%
    FormulaFunc/f0/Func-4           40.0B ± 0%
    FormulaFunc/f1/Eval-4           48.0B ± 0%
    FormulaFunc/f1/Func-4           48.0B ± 0%
    FormulaFunc/f2/Eval-4           48.0B ± 0%
    FormulaFunc/f2/Func-4           48.0B ± 0%
    FormulaFunc/f3/Eval-4           48.0B ± 0%
    FormulaFunc/f3/Func-4           48.0B ± 0%
    
    name                            allocs/op
    Formula/42.0/Eval-4              3.00 ± 0%
    Formula/42.0/Func-4              2.00 ± 0%
    Formula/F64/Eval-4               3.00 ± 0%
    Formula/F64/Func-4               2.00 ± 0%
    Formula/2*F64/Eval-4             3.00 ± 0%
    Formula/2*F64/Func-4             2.00 ± 0%
    Formula/math.Abs(2*F64)/Eval-4   3.00 ± 0%
    Formula/math.Abs(2*F64)/Func-4   2.00 ± 0%
    FormulaFunc/f0/Eval-4            2.00 ± 0%
    FormulaFunc/f0/Func-4            2.00 ± 0%
    FormulaFunc/f1/Eval-4            2.00 ± 0%
    FormulaFunc/f1/Func-4            2.00 ± 0%
    FormulaFunc/f2/Eval-4            2.00 ± 0%
    FormulaFunc/f2/Func-4            2.00 ± 0%
    FormulaFunc/f3/Eval-4            2.00 ± 0%
    FormulaFunc/f3/Func-4            2.00 ± 0%

--- groot/rtree/example_reader_test.go
+++ groot/rtree/example_reader_test.go
@@ -262,3 +262,74 @@ func ExampleReader_withFormula() {
 	// evt[2]: 3, 3.3, tres -> 3433 3433 | "one": 3, "two": 3.3, "three": tres
 	// evt[3]: 4, 4.4, quatro -> 4644 4644 | "one": 4, "two": 4.4, "three": quatro
 }
+
+func ExampleReader_withFormulaFunc() {
+	f, err := groot.Open("../testdata/simple.root")
+	if err != nil {
+		log.Fatalf("could not open ROOT file: %+v", err)
+	}
+	defer f.Close()
+
+	o, err := f.Get("tree")
+	if err != nil {
+		log.Fatalf("could not retrieve ROOT tree: %+v", err)
+	}
+	t := o.(rtree.Tree)
+
+	var (
+		data struct {
+			V1 int32   `groot:"one"`
+			V2 float32 `groot:"two"`
+			V3 string  `groot:"three"`
+		}
+		rvars = rtree.ReadVarsFromStruct(&data)
+	)
+
+	r, err := rtree.NewReader(t, rvars)
+	if err != nil {
+		log.Fatalf("could not create tree reader: %+v", err)
+	}
+	defer r.Close()
+
+	f64, err := r.FormulaFunc(
+		[]string{"one", "two", "three"},
+		func(v1 int32, v2 float32, v3 string) float64 {
+			return float64(v2*10) + float64(1000*v1) + float64(100*len(v3))
+		},
+	)
+	if err != nil {
+		log.Fatalf("could not create formula: %+v", err)
+	}
+
+	fstr, err := r.FormulaFunc(
+		[]string{"one", "two", "three"},
+		func(v1 int32, v2 float32, v3 string) string {
+			return fmt.Sprintf(
+				"%q: %v, %q: %v, %q: %v",
+				"one", v1, "two", v2, "three", v3,
+			)
+		},
+	)
+	if err != nil {
+		log.Fatalf("could not create formula: %+v", err)
+	}
+
+	f1 := f64.Func().(func() float64)
+	f2 := fstr.Func().(func() string)
+
+	err = r.Read(func(ctx rtree.RCtx) error {
+		v64 := f1()
+		str := f2()
+		fmt.Printf("evt[%d]: %v, %v, %v -> %g %g | %s\n", ctx.Entry, data.V1, data.V2, data.V3, f64.Eval(), v64, str)
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("could not process tree: %+v", err)
+	}
+
+	// Output:
+	// evt[0]: 1, 1.1, uno -> 1311 1311 | "one": 1, "two": 1.1, "three": uno
+	// evt[1]: 2, 2.2, dos -> 2322 2322 | "one": 2, "two": 2.2, "three": dos
+	// evt[2]: 3, 3.3, tres -> 3433 3433 | "one": 3, "two": 3.3, "three": tres
+	// evt[3]: 4, 4.4, quatro -> 4644 4644 | "one": 4, "two": 4.4, "three": quatro
+}
