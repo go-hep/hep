350be89d8e9ee2ee51fa928e170afa0ef1fd96b2
rmadar <romain.madar@gmail.com>
2020-06-14 22:51:34 +0200

    gofmt and cleaning

--- hplot/example_binerrband_test.go
+++ hplot/example_binerrband_test.go
@@ -5,38 +5,37 @@
 package hplot_test
 
 import (
-	"log"
 	"image/color"
-	
+	"log"
+
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
-	
+
 	"go-hep.org/x/hep/hplot"
 )
 
 // An example of making a colored band plot
 func ExampleBinnedErrBand() {
 
-	// Binning 
+	// Binning
 	bins := newBinning(10, 0, 10)
 
 	// Y values and errors
 	vals := make(plotter.Values, 10)
 	errs := make(plotter.YErrors, 10)
 	for i := range bins {
-		vals[i] = float64(i+1)
+		vals[i] = float64(i + 1)
 		errs[i].Low = 0.1 * vals[i]
 		errs[i].High = 0.1 * vals[i]
 	}
 
 	// Set 5th bin to zero
 	vals[4], errs[4].Low, errs[4].High = 0, 0, 0
-	
+
 	// Binned error band
 	b := hplot.NewBinnedErrBand(bins, vals, errs)
 	b.FillColor = color.NRGBA{B: 180, A: 200}
-	b.LineStyle.Color = color.NRGBA{R: 180, A: 200}
-		
+
 	// Create a new plot and add b
 	p := hplot.New()
 	p.Title.Text = "Binned Error Band"
@@ -48,7 +47,7 @@ func ExampleBinnedErrBand() {
 	err := p.Save(10*vg.Centimeter, -1, "testdata/binnederrband.png")
 	if err != nil {
 		log.Fatalf("error: %+v", err)
-	}	
+	}
 }
 
 // FIX-ME[rmadar]: once the proper type for b.Bins
@@ -57,7 +56,7 @@ func ExampleBinnedErrBand() {
 func newBinning(n int, xmin, xmax float64) [][2]float64 {
 	res := make([][2]float64, n)
 	dx := (xmax - xmin) / float64(n)
-	for i := 0 ; i < n ; i++ {
+	for i := 0; i < n; i++ {
 		lo := xmin + float64(i)*dx
 		hi := lo + dx
 		res[i] = [2]float64{lo, hi}
