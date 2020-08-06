217ec82b5a3e38e6ab43959b0698fd728293300c
rmadar <romain.madar@gmail.com>
2020-08-06 16:30:27 +0200

    Implement part of the comments (AutoAdjust doent work yet)

--- hplot/example_label_test.go
+++ hplot/example_label_test.go
@@ -5,30 +5,19 @@
 package hplot_test
 
 import (
+	"fmt"
 	"log"
 
+	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 
 	"go-hep.org/x/hep/hplot"
 )
 
 func ExampleLabel() {
 
-	// Label definition
-	l1 := hplot.Label{
-		Text: "You are in ...",
-		X:    -0.5, Y: 0.5,
-	}
-	l2 := hplot.Label{
-		Text: "... the right place",
-		X:    0.5, Y: -0.5,
-	}
-	l3 := hplot.Label{
-		Text: "This is the middle",
-		X:    0.5, Y: 0.5,
-		Normalized: true}
-
-	// New plot
+	// Creating a new plot
 	p := hplot.New()
 	p.Title.Text = "Plot labels"
 	p.X.Min = -1
@@ -36,10 +25,37 @@ func ExampleLabel() {
 	p.Y.Min = -1
 	p.Y.Max = 1
 
-	// Adding the labels
+	// Default labels
+	l1 := hplot.NewLabel(-0.8, 0.5, "Default label.")
 	p.Add(l1)
-	p.Add(l2)
+
+	// Label with normalized coordinates.
+	l3 := hplot.NewLabel(0.5, 0.5, "Label with relative coordinates.",
+		hplot.WithNormalized(true),
+	)
 	p.Add(l3)
+
+	// Label with normalized coordinates and auto-adjustement.
+	l4 := hplot.NewLabel(0.95, 0.95, "Label at the canvas edge, with AutoAdjust",
+		hplot.WithNormalized(true),
+		//hplot.WithAutoAdjust(true),
+	)
+	p.Add(l4)
+
+	// Label with a customed TextStyle
+	usrFont, err := vg.MakeFont("Courier-Bold", 12)
+	if err != nil {
+		panic(fmt.Errorf("could not create font (Courier-Bold, 12): %w", err))
+	}
+	sty := draw.TextStyle{
+		Color: plotutil.Color(2),
+		Font:  usrFont,
+	}
+	l5 := hplot.NewLabel(0.0, 0.1, "Label with a user-defined font",
+		hplot.WithTextStyle(sty),
+		hplot.WithNormalized(true),
+	)
+	p.Add(l5)
 
 	// Save the plot to a PNG file.
 	if err := p.Save(6*vg.Inch, -1, "testdata/label_plot.png"); err != nil {
--- hplot/label.go
+++ hplot/label.go
@@ -5,6 +5,7 @@
 package hplot
 
 import (
+	"fmt"
 	"image/color"
 	"math"
 
@@ -20,11 +21,65 @@ type Label struct {
 	TextStyle draw.TextStyle // Text style of the label
 
 	// Normalized indicates whether the label position
-	//is in data coordinates  or normalized with regard
+	// is in data coordinates or normalized with regard
 	// to the canvas space.
 	// When normalized, the label position is assumed
-	// to fall in the [0, 1] interval.
+	// to fall in the [0, 1] interval. If true, NewLabel
+	// panics if x or y are outside [0, 1].
+	// Normalized is false by default.
 	Normalized bool
+
+	// AutoAdjust enables auto adjustment of the label
+	// position, when Normalized is true and when x
+	// and/or y are close to 1 and the label is partly
+	// outside the canvas. If false and the label doesn't
+	// fit in the canvas, an error is returned.
+	// AutoAdjust is false by default.
+	AutoAdjust bool
+}
+
+// NewLabel creates a new label value from x, y.
+func NewLabel(x, y float64, txt string, opts ...LabelOption) Label {
+
+	// FIXME[rmadar]: default style to be handle properly
+	//                once hplot.Style is ready.
+	font, fSize := plotter.DefaultFont, plotter.DefaultFontSize
+	defaultFont, err := vg.MakeFont(font, fSize)
+	if err != nil {
+		panic(fmt.Errorf("hplot: could not create font (%s, %v): %w", font, fSize, err))
+	}
+	style := draw.TextStyle{
+		Color: color.Black,
+		Font:  defaultFont,
+	}
+
+	// Default config
+	cfg := &labelConfig{
+		TextStyle:  style,
+		Normalized: false,
+		AutoAdjust: false,
+	}
+
+	// User-defined options
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	// Sanity check
+	if cfg.Normalized && (x < 0 || x > 1 || y < 0 || y > 1) {
+		panic("hplot: normalized label position is outside [0,1]")
+	}
+
+	// Return the configured Label
+	return Label{
+		Text:       txt,
+		X:          x,
+		Y:          y,
+		TextStyle:  cfg.TextStyle,
+		Normalized: cfg.Normalized,
+		AutoAdjust: cfg.Normalized,
+	}
+
 }
 
 // Plot implements the Plotter interface,
@@ -37,7 +92,6 @@ func (lbl Label) Plot(c draw.Canvas, p *plot.Plot) {
 // DataRange returns the minimum and maximum x and
 // y values, implementing the plot.DataRanger interface.
 func (lbl Label) DataRange() (xmin, xmax, ymin, ymax float64) {
-
 	if lbl.Normalized {
 		return math.Inf(+1), math.Inf(-1), math.Inf(+1), math.Inf(-1)
 	}
@@ -49,40 +103,40 @@ func (lbl Label) DataRange() (xmin, xmax, ymin, ymax float64) {
 // GlyphBoxes returns a GlyphBoxe, corresponding
 // to the label, implementing the plot.GlyphBoxer interface.
 func (lbl Label) GlyphBoxes(p *plot.Plot) []plot.GlyphBox {
-
 	if lbl.Normalized {
-		// FIXME[rmadar]: this crashes and I don't understand why.
 		return []plot.GlyphBox{
 			{X: lbl.X, Y: lbl.Y, Rectangle: lbl.TextStyle.Rectangle(lbl.Text)},
 		}
 	}
-
 	return lbl.labels(draw.Canvas{}, p).GlyphBoxes(p)
 }
 
 // Internal helper function to get plotter.Labels type.
 func (lbl *Label) labels(c draw.Canvas, p *plot.Plot) *plotter.Labels {
 
-	if lbl.TextStyle == (draw.TextStyle{}) {
-		// FIXME[rmadar]: implement the hplot.DefaultFont
-		//                and hplot.DefaultFontSize.
-		defaultFont, err := vg.MakeFont(plotter.DefaultFont, plotter.DefaultFontSize)
-		if err != nil {
-			panic("impossible to make font.")
-		}
-
-		lbl.TextStyle = draw.TextStyle{
-			Color: color.Black,
-			Font:  defaultFont,
-		}
-	}
-
 	x := lbl.X
 	y := lbl.Y
 	if lbl.Normalized {
-		dc := p.DataCanvas(c)
-		x = float64(dc.X(x))
-		y = float64(dc.Y(y))
+
+		// Check wether the label fits in the canvas
+		txtBox := lbl.TextStyle.Rectangle(lbl.Text)
+		xMax := p.X.Norm(float64(txtBox.Max.X))
+		yMax := p.Y.Norm(float64(txtBox.Max.Y))
+		if xMax > 1 || yMax > 1 {
+			//if lbl.AutoAdjust {
+			//	x = x - (xMax - 1)
+			//	y = y - (yMax - 1)
+			//} else {
+			//panic("hplot: labels fall outside canvas")
+			//}
+		}
+
+		// Turn relative into absolute coordinates
+		dataCoord := func(xrel, xmin, xmax float64) float64 {
+			return xmin + xrel*(xmax-xmin)
+		}
+		x = dataCoord(x, p.X.Min, p.X.Max)
+		y = dataCoord(y, p.Y.Min, p.Y.Max)
 	}
 
 	xyL := plotter.XYLabels{
@@ -92,12 +146,45 @@ func (lbl *Label) labels(c draw.Canvas, p *plot.Plot) *plotter.Labels {
 
 	lbls, err := plotter.NewLabels(xyL)
 	if err != nil {
-		panic("cannot create plotter.Labels")
+		panic(fmt.Errorf("hplot: could not create labels: %w", err))
 	}
 
 	lbls.TextStyle = []draw.TextStyle{lbl.TextStyle}
 
 	return lbls
+}
+
+type labelConfig struct {
+	TextStyle  draw.TextStyle
+	Normalized bool
+	AutoAdjust bool
+}
+
+// Label option handles various options to
+// configure the label.
+type LabelOption func(cfg *labelConfig)
+
+// WithTextStyle specifies the text style of the label.
+func WithTextStyle(style draw.TextStyle) LabelOption {
+	return func(cfg *labelConfig) {
+		cfg.TextStyle = style
+	}
+}
+
+// WithTextStyle specifies whether the coordinate are
+// normalized to the canvas size.
+func WithNormalized(norm bool) LabelOption {
+	return func(cfg *labelConfig) {
+		cfg.Normalized = norm
+	}
+}
+
+// WithTextStyle specifies whether the coordinate are
+// normalized to the canvas size.
+func WithAutoAdjust(auto bool) LabelOption {
+	return func(cfg *labelConfig) {
+		cfg.AutoAdjust = auto
+	}
 }
 
 var (
