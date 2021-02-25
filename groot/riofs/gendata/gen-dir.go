eda01ec6b22eb25ac277f7507b2285b39423b31f
Sebastien Binet <binet@cern.ch>
2021-02-25 15:00:55 +0100

    all: update license blurb
    Signed-off-by: Sebastien Binet <binet@cern.ch>

--- groot/riofs/gendata/gen-dir.go
+++ groot/riofs/gendata/gen-dir.go
@@ -1,9 +1,9 @@
-//go:build ignore
-// +build ignore
-
 // Copyright ©2018 The go-hep Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+//go:build ignore
+// +build ignore
 
 package main
 
