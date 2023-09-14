312405d8a8f3a0cc4a4176f998d572371addf5ff
Sebastien Binet <binet@cern.ch>
2023-09-14 11:46:50 +0200

    groot/rjson: add support for slices of RSlicer
    Signed-off-by: Sebastien Binet <binet@cern.ch>

--- groot/rjson/rjson.go
+++ groot/rjson/rjson.go
@@ -10,11 +10,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"reflect"
 
 	"go-hep.org/x/hep/groot/rbytes"
-	"go-hep.org/x/hep/groot/rdict"
 	"go-hep.org/x/hep/groot/root"
 )
 
@@ -27,47 +25,6 @@ func Marshal(o root.Object) ([]byte, error) {
 	}
 
 	panic(fmt.Errorf("not implemented for %T", o))
-
-	vers := -1
-	if o, ok := o.(interface{ RVersion() int }); ok {
-		vers = o.RVersion()
-	}
-	log.Printf("--> marshal %T", o)
-	si, err := rdict.StreamerInfos.StreamerInfo(o.Class(), vers)
-	if err != nil {
-		return nil, fmt.Errorf("could not find streamer info for %T: %w", o, err)
-	}
-
-	w := new(bytes.Buffer)
-	w.WriteByte('{')
-	for i, elt := range si.Elements() {
-		log.Printf("elt: %+v", elt)
-		switch elt := elt.(type) {
-		case *rdict.StreamerBase:
-			log.Printf(">>> %+v", elt)
-		}
-		if i > 0 {
-			w.WriteByte(',')
-		}
-		fmt.Fprintf(w, "%q: %q", "_typename", elt.Name())
-	}
-	w.WriteByte('}')
-
-	rv := reflect.Indirect(reflect.ValueOf(o))
-	for i := 0; i < rv.NumField(); i++ {
-		rf := rv.Field(i)
-		ft := rv.Type().Field(i)
-		var (
-			vv   any
-			name = ft.Name
-		)
-		if ft.IsExported() {
-			vv = rf.Interface()
-		}
-		log.Printf("field[%d]: %q %+v (%s)", i, name, vv, ft.Type.Name())
-	}
-
-	return w.Bytes(), nil
 }
 
 type encoder struct {
@@ -76,6 +33,53 @@ type encoder struct {
 
 func newEncoder(w io.Writer) *encoder {
 	return &encoder{w: w}
+}
+
+func (enc *encoder) encode(v any) error {
+	if v, ok := v.(rbytes.RSlicer); ok {
+		return enc.Encode(v)
+	}
+
+	var (
+		err error
+		w   = enc.w
+		rv  = reflect.Indirect(reflect.ValueOf(v))
+	)
+
+	switch rv.Kind() {
+	case reflect.Slice:
+		_, err = w.Write([]byte("["))
+		if err != nil {
+			return err
+		}
+		for i := 0; i < rv.Len(); i++ {
+			if i > 0 {
+				_, err := w.Write([]byte(","))
+				if err != nil {
+					return err
+				}
+			}
+			err = enc.encode(rv.Index(i).Interface())
+			if err != nil {
+				return err
+			}
+		}
+		_, err = w.Write([]byte("]"))
+		if err != nil {
+			return err
+		}
+	default:
+		raw, err := json.Marshal(v)
+		if err != nil {
+			return err
+		}
+		_, err = w.Write(raw)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (enc *encoder) Encode(v rbytes.RSlicer) error {
@@ -91,25 +95,7 @@ func (enc *encoder) Encode(v rbytes.RSlicer) error {
 		if err != nil {
 			return err
 		}
-		if v, ok := m.Value.(rbytes.RSlicer); ok {
-			err := enc.Encode(v)
-			if err != nil {
-				return err
-			}
-			continue
-		}
-		if v := reflect.Indirect(reflect.ValueOf(m.Value)); v.Kind() == reflect.Slice && v.Len() == 0 {
-			_, err = w.Write([]byte("[]"))
-			if err != nil {
-				return err
-			}
-			continue
-		}
-		v, err := json.Marshal(m.Value)
-		if err != nil {
-			return err
-		}
-		_, err = w.Write(v)
+		err = enc.encode(m.Value)
 		if err != nil {
 			return err
 		}
